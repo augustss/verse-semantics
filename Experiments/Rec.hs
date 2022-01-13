2b24baa56a9fea3474620c4e7a44e6b6f9e0aa6d
Simon Peyton Jones <simon.peytonjones@gmail.com>
2022-01-13 23:04:47 +0000

    More from Simon

--- Experiments/Rec.hs
+++ Experiments/Rec.hs
@@ -14,9 +14,62 @@ x ==. y | x == y = [x]
 class (Monad m) => MonadFix m where
         mfix :: (a -> m a) -> m a
 
-mfixList :: (a -> [a]) -> [a]
-mfixList f = let (x:xs) = f x in x:xs
+instance MonadFix [] where
+    mfix f = case fix (f . head) of
+               []    -> []
+               (x:_) -> x : mfix (tail . f)
 
+mfixList :: (a -> [a]) -> [a]
+mfixList f = let xs = f (head xs)
+             in
+             case xs of
+               []    -> []
+               (x:_) -> x : mfixList (tail . f)
+
+-- Unrolling this shows us what happens:
+mfixList f = let xs1 = f (head xs1) in
+             case xs1 of {
+               []     -> [] ;
+               (x1:_) -> x :
+
+             let xs2 = tail (f (head xs2)) in
+             case xs2 of {
+               []    -> [] ;
+               (x2:_) -> x2 :
+
+             let xs3 = tail (tail (f (head xs3))) in
+             case xs3 of {
+               []    -> [] ;
+               (x3:_) -> x3 :
+
+             ....
+
+Another way to write it:
+
+mfixList :: forall a. (a -> [a]) -> [a]
+mfixList f = go 0
+ where
+   go :: Int -> [a]
+   go n = let mx :: Maybe a
+              mx = indexM n (f (the mx))
+          in case mx of
+             Nothing -> []
+             Just x  -> x : go (n+1)
+   -- Assuming result has at least i elements, the i'th element of
+   -- the result list is xi where
+   --     xi = the (indexM n (f xi))
+   -- In each iteration we apply f afresh to a thunk, and ignore
+   -- all but the i'th element of the result.
+
+indexM :: Int -> [a] -> Maybe a
+-- Get the i'th element, or Nothing if it has too few elements
+indexM _ []     = Nothing
+indexM 0 (x:_)  = Just x
+indexM n (_:xs) = indexM (n-1) xs
+
+the :: Maybe a -> a
+the Nothing  = error "Deadlock"
+the (Just x) = x
 -}
 
 -- [(1,2),(1,3),(2,2),(2,3)]
