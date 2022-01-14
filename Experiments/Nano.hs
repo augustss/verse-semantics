4ae9618b2c0734cb521b235904732cf4e7c26506
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-01-14 13:48:08 +0100

    Change eval to return a list of Env->Value functions.

--- Experiments/Nano.hs
+++ Experiments/Nano.hs
@@ -28,35 +28,52 @@ instance Show Value where
 
 type Env = [(Ident, Value)]
 
+type Ext = (Ident, Value)  -- Environment extension in a Def.  Only one variable for now
+type Res = Ext -> Value
+
 ---------------------
 --      Semantics
 ---------------------
 
-eval :: Exp -> Env -> [Value]
+eval :: Exp -> Env -> [Res]
 eval (Var i) rho = evalVar i rho
-eval (Con k) _ = [VCon k]
+eval (Con k) _ = [const $ VCon k]
 eval (Alt e1 e2) rho = eval e1 rho ++ eval e2 rho
 eval Fail _ = []
-eval (Equal e1 e2) rho = [ v1 | v1 <- eval e1 rho, v2 <- eval e2 rho, v1 == v2]
-eval (Pair e1 e2) rho = [ VPair v1 v2 | v1 <- eval e1 rho, v2 <- eval e2 rho ]
-eval (Fst e1) rho = map vfst (eval e1 rho)
+eval (Equal e1 e2) rho =
+  [ const v1 | fv1 <- eval e1 rho, fv2 <- eval e2 rho,
+               let v1 = fv1 empty, let v2 = fv2 empty,
+               v1 == v2 ]
+eval (Pair e1 e2) rho = [ \ext -> VPair (fv1 ext) (fv2 ext) | fv1 <- eval e1 rho, fv2 <- eval e2 rho ]
+eval (Fst e1) rho = map (vfst .) (eval e1 rho)
   where vfst (VPair v _) = v
         vfst v = error $ "vfst " ++ show v
-eval (Snd e1) rho = map vsnd (eval e1 rho)
+eval (Snd e1) rho = map (vsnd .) (eval e1 rho)
   where vsnd (VPair _ v) = v
         vsnd v = error $ "vsnd " ++ show v
-eval (Succ e1) rho = map vsucc (eval e1 rho)
+eval (Succ e1) rho = map (vsucc .) (eval e1 rho)
   where vsucc (VCon i) = VCon (succ i)
         vsucc v = error $ "vsucc " ++ show v
-  
-eval (Def x r e) rho = [ v | xv <- eval r rho', v <- eval e ((x, xv) : rho) ]
-  where rho' = (x, head $ eval r rho') : rho
+eval (Def x r b) rho =
+  -- Why does it work with const?
+  [ const v | xv <- xvs, v <- evalV b ((x,xv):rho) ]
+  where rfs = eval r rho
+        xvs = aux rfs xvs
+        aux [] _ = []
+        aux (f:fs) ~(v:vs) = f (x,v) : aux fs vs
 
-evalVar :: Ident -> Env -> [Value]
-evalVar i rho =
-  case lookup i rho of
+evalVar :: Ident -> Env -> [Res]
+evalVar i rho = [\ ext ->
+  case lookup i (ext:rho) of
     Nothing -> error $ "not found " ++ show i
-    Just v -> [v]
+    Just v -> v
+  ]
+
+empty :: Ext
+empty = ("",undefined)
+
+evalV :: Exp -> Env -> [Value]
+evalV e rho = [f empty | f <- eval e rho]
 
 ---------------------
 --      Tests
@@ -80,19 +97,19 @@ ex1a = Def ixy (Pair (Con 1 `Alt` Con 2) (Con 2 `Alt` Con 3)) (Pair (Snd xy) (Fs
 -- ex2:   def { (x,y) = ( (y+1)|3, 1|2 ) in (x,y)
 -- Tim            [(2,1),(3,2),(3,1),(3,2)]
 -- Rec            [(2,1),(3,2),(3,1),(3,2)]
--- eval ex2 [] == [(2,1),(2,2),(3,1),(3,2)]  -- WRONG
+-- eval ex2 [] == [(2,1),(3,2),(3,1),(3,2)]
 ex2 = Def ixy (Pair (Succ y `Alt` Con 3) (Con 1 `Alt` Con 2)) xy
 
 -- ex5: def { xy = ((y=4)|2), (3|4) } in xy
 -- Tim            [(4,4),(2,3),(2,4)]
 -- Rec            bottom
--- eval ex5 [] == bottom (eval loops)
+-- eval ex5 [] == bottom (error, xy unbound)
 ex5 = Def ixy (Pair (Equal y (Con 4) `Alt` Con 2) (Con 3 `Alt` Con 4)) xy
 
 -- ex7: def { x = 1 | x | 2 } in x
 -- Tim            [1,..,2]
 -- Rec            [1,       -- loops, but length ex7 == 3
--- eval ex7 [] == [1,1,2]   -- WRONG
+-- eval ex7 [] == [1,       -- loops, but length ex7 == 3
 ex7 = Def "x" (Con 1 `Alt` Var "x" `Alt` Con 2) (Var "x")
 
 -- ex8: def { x = 1|2 } in def { y = 3|4 } in (x, y)
