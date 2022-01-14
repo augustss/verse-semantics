bdb2877cd0bdf32b2991f8982005f3b634b8d0a6
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-01-14 21:23:42 +0100

    Change Res to distinguish between delayed evaluation and not.

--- Experiments/Nano.hs
+++ Experiments/Nano.hs
@@ -58,51 +58,68 @@ instance Show Value where
 type Env = [(Ident, Value)]
 
 type Ext = (Ident, Value)  -- Environment extension in a Def.  Only one variable for now
-type Res = Ext -> Value
+
+data Res = Delay (Ext -> Value) | Done Value  -- (Done v) is equivalent to (Delay (\_. v))
 
 ---------------------
 --      Semantics
 ---------------------
 
+-- NOTE: This assumes all variable names are unique.
+-- If the are not unique the bound variable from a Def might be found
+-- in the outer environment.
+-- A fix for this would be to delete x from rho in the Def case.
+
 eval :: Exp -> Env -> [Res]
 eval (Var i) rho = evalVar i rho
-eval (Con k) _ = [const $ VCon k]
+eval (Con k) _ = [Done $ VCon k]
 eval (Alt e1 e2) rho = eval e1 rho ++ eval e2 rho
 eval Fail _ = []
 eval (Equal e1 e2) rho =
-  [ const v1 | fv1 <- eval e1 rho, fv2 <- eval e2 rho,
-               let v1 = fv1 empty, let v2 = fv2 empty,
-               v1 == v2 ]
-eval (Pair e1 e2) rho = [ \ext -> VPair (fv1 ext) (fv2 ext) | fv1 <- eval e1 rho, fv2 <- eval e2 rho ]
-eval (Fst e1) rho = map (vfst .) (eval e1 rho)
+  [ Done v1 | r1 <- eval e1 rho, r2 <- eval e2 rho,
+            let v1 = withExt empty r1, let v2 = withExt empty r2,
+            v1 == v2 ]
+eval (Pair e1 e2) rho = [ lift2 VPair fv1 fv2 | fv1 <- eval e1 rho, fv2 <- eval e2 rho ]
+eval (Fst e1) rho = map (lift1 vfst) (eval e1 rho)
   where vfst (VPair v _) = v
         vfst v = error $ "vfst " ++ show v
-eval (Snd e1) rho = map (vsnd .) (eval e1 rho)
+eval (Snd e1) rho = map (lift1 vsnd) (eval e1 rho)
   where vsnd (VPair _ v) = v
         vsnd v = error $ "vsnd " ++ show v
-eval (Plus e1 e2) rho = [ \ext -> vplus (fv1 ext) (fv2 ext) | fv1 <- eval e1 rho, fv2 <- eval e2 rho ]
+eval (Plus e1 e2) rho = [ lift2 vplus fv1 fv2 | fv1 <- eval e1 rho, fv2 <- eval e2 rho ]
   where vplus (VCon i1) (VCon i2) = VCon (i1 + i2)
         vplus v1 v2 = error $ "vplus " ++ show (v1, v2)
 eval (Def x r b) rho =
-  -- Why does it work with const?
-  [ const v | xv <- xvs, v <- evalV b ((x,xv):rho) ]
+  [ Done v | xv <- xvs, v <- evalV b ((x,xv):rho) ]
   where rfs = eval r rho
         xvs = aux rfs xvs
         aux [] _ = []
-        aux (f:fs) ~(v:vs) = f (x,v) : aux fs vs
+        aux (f:fs) ~(v:vs) = withExt (x,v) f : aux fs vs
 
 evalVar :: Ident -> Env -> [Res]
-evalVar i rho = [\ ext ->
-  case lookup i (ext:rho) of
-    Nothing -> error $ "not found " ++ show i
-    Just v -> v
-  ]
+evalVar i rho =
+  case lookup i rho of
+    Nothing -> [Delay $ \ (y, w) -> if i == y then w else error $ "not found " ++ show i]
+    Just v -> [Done v]
+
+lift1 :: (Value -> Value) -> Res -> Res
+lift1 g (Delay f) = Delay (g . f)
+lift1 g (Done v) = Done (g v)
+
+lift2 :: (Value -> Value -> Value) -> Res -> Res -> Res
+lift2 g (Delay f1) (Delay f2) = Delay (\ ext -> f1 ext `g` f2 ext)
+lift2 g (Delay f1) (Done v2) = Delay (\ ext -> f1 ext `g` v2)
+lift2 g (Done v1) (Delay f2) = Delay (\ ext -> v1 `g` f2 ext)
+lift2 g (Done v1) (Done v2) = Done (v1 `g` v2)
+
+withExt ext (Delay f) = f ext
+withExt _ (Done v) = v
 
 empty :: Ext
 empty = ("",undefined)
 
 evalV :: Exp -> Env -> [Value]
-evalV e rho = [f empty | f <- eval e rho]
+evalV e rho = [withExt empty f | f <- eval e rho]
 
 ---------------------
 --      Tests
