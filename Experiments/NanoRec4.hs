cfec5a5baafa21b1e797f6d8e5488369f90d98fb
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-01-24 19:22:02 +0100

    Update with fixes from NanoRec3.

--- Experiments/NanoRec4.hs
+++ Experiments/NanoRec4.hs
@@ -5,6 +5,7 @@ import Data.List
 import Data.Maybe
 import Data.String
 import Debug.Trace
+import Ex
 
 ---------------------
 --      Expressions
@@ -38,7 +39,7 @@ data Exp = Var Ident
          | Error  -- to test strictness
   deriving (Show)
 
-infix 1 :=
+infix 2 :=
 
 ---------------------
 --      Sugar
@@ -51,11 +52,11 @@ instance Num Exp where
 instance IsString Exp where
   fromString = Var
 
-infixl 3 |||
+infixl 4 |||
 (|||) :: Exp -> Exp -> Exp
 (|||) = Alt
 
-infixl 2 #
+infixl 3 #
 (#) :: Exp -> Exp -> Exp
 (#) = Pair
 
@@ -71,12 +72,12 @@ pattern Snd e = Sel e 1
 pattern Pair e1 e2 = Array [e1, e2]
 
 -- Sequencing, evaluate both and return second
-infixl 0 `semi`
+infixl 1 `semi`
 semi :: Exp -> Exp -> Exp
 semi x y = Snd (Pair x y)
 
 -- Sequencing, evaluate both and return first
-infix 0 `wher`
+infix 1 `wher`
 wher :: Exp -> Exp -> Exp
 wher x y = Fst (Pair x y)
 
@@ -182,7 +183,7 @@ eval (For e1 e2) rho = map mkArr $ sequence
   | (ext1, _) <- eval e1 rho
   -- ext1 has delay for its own variables
   -- Note the recursice use of ext1', without it we would need several passes.
-  , let ext1' = [(x, withExt ext1' v) | (x, v) <- ext1 ]
+  , let ext1' = tieKnotExt ext1
   ]
   where mkArr :: [Res] -> Res
         mkArr rs = (empty, Done $ VArray $ map snd rs)
@@ -194,27 +195,36 @@ evalVar i rho = case lookupEnv i rho of
                   Just v  -> v
                   Nothing -> Delay (dly "Var" [i]) (evalVar i)
 
--- WRONG: after a Delay has been resolved, it may contain further delays.
--- See test26
 tieKnot :: [Res] -> [Res]
---tieKnot vs | trace ("tieKnot: " ++ show vs) False = undefined
-tieKnot vs = [ (empty, withExt ext v) | (ext, v) <- vs ]
+tieKnot vs = [ (empty, withExtL (tieKnotExt ext) v)
+             | (ext, v) <- vs
+             ]
 
-withExt :: Ext -> Lenient -> Lenient
-withExt ext =
-  let
-     withExtV v@(VInt _) = v
-     withExtV (VArray ls) = VArray (map withExtL ls)
-     withExtL (Done v) = Done (withExtV v)
-     withExtL (Delay _ f) = f ext
-  in withExtL
+withExtL :: Env -> Lenient -> Lenient
+withExtL ext (Delay s f) = f ext
+withExtL ext (Done v)    = Done (withExtV ext v)
+  where
+     withExtV :: Env -> Value -> Value
+     withExtV _ v@(VInt _)      = v
+     withExtV ext (VArray ls) = VArray (map (withExtL ext) ls)
 
+tieKnotExt :: Ext -> Ext
+tieKnotExt ext = rec_ext
+  -- Here is where we tie the knot.  Consider an example like
+  --   x:=y; y:=z; z:=3; z
+  -- we get an 'ext' that binds x and y to Delays, and we
+  -- must resolve both at once when we tie the knot
+  where rec_ext = mapEnv (withExtL rec_ext) ext
+ 
 ---------------------
 --      Auxiliary semantic operations
 ---------------------
 
 extendEnv :: Ident -> Lenient -> Env -> Env
 extendEnv x v rho = (x,v) : rho
+
+mapEnv :: (Lenient -> Lenient) -> Env -> Env
+mapEnv f env = [ (i, f v) | (i,v) <- env ]
 
 unionEnv :: Ext -> Env -> Env
 -- If both envs bind the same variable 'ext1' wins
@@ -246,10 +256,6 @@ liftL2 _ g (Done v1) (Done v2)   = Done (v1 `g` v2)
 dly :: String -> [String] -> String
 dly s ss = s ++ "(" ++ intercalate "," ss ++ ")"
 
----------------------
---      Tests
----------------------
-
 evalTop :: Exp -> [Res]
 evalTop e = tieKnot (eval e [])
 
@@ -261,80 +267,121 @@ ev e = map get (evalTop e)
                            Done v -> v
                            Delay {} -> error "Top level delay"
 
--- Works [1,2]
-test1 = 1 ||| 2
+---------------------
+--      Tests
+---------------------
 
--- Works [2,3,3,4]
-test2 = (1 ||| 2) + (1 ||| 2)
+ok :: (Show a) => String -> a -> Exp -> Ex String
+ok n r e = Ex n (Just $ show r) (show $ ev e)
 
--- Works [2,4]
-test3 = ("x" := 1 ||| 2) + "x"
+bad :: String -> Exp -> Ex String
+bad n e = Ex n Nothing (show $ ev e)
+
+test1 = ok "test1" [1,2] $
+  1 ||| 2
+
+test2 = ok "test2" [2,3,3,4] $
+  (1 ||| 2) + (1 ||| 2)
+
+test3 = ok "test3" [2,4] $
+  ("x" := 1 ||| 2) + "x"
 
 -- Should fail, since variables in ||| do not escape
 -- Fails
-test4 = (("x" := 1) ||| 2) + "x"
+test4 = bad "test4" $
+  (("x" := 1) ||| 2) + "x"
 
--- works [(4,(1,3)),(5,(1,4)),(5,(2,3)),(6,(2,4))]
-test5 = ("x" := 1 ||| 2) + ("y" := 3 ||| 4) # ("x" # "y")
+test5 = ok "test5" [(4,(1,3)),(5,(1,4)),(5,(2,3)),(6,(2,4))] $
+  ("x" := 1 ||| 2) + ("y" := 3 ||| 4) # ("x" # "y")
 
--- works [(2,(1,1)),(5,(1,4)),(4,(2,2)),(6,(2,4))]
-test6 = ("x" := 1 ||| 2) + ("y" := "x" ||| 4) # ("x" # "y")
+test6 = ok "test6" [(2,(1,1)),(5,(1,4)),(4,(2,2)),(6,(2,4))] $
+  ("x" := 1 ||| 2) + ("y" := "x" ||| 4) # ("x" # "y")
 
--- works [4]
-test7 = ("x" := 1 ||| 2) + ("x" === 2)
+test7 = ok "test7" [4] $
+  ("x" := 1 ||| 2) + ("x" === 2)
 
--- works [(1,1),(2,2)]
-test8 = Pair "x" ("x" := 1 ||| 2)
+test8 = ok "test8" [(1,1),(2,2)] $
+  Pair "x" ("x" := 1 ||| 2)
 
--- Works [(7,(1,1)),(7,(2,2)),(1,(1,1)),(2,(2,2))]
-test9 = Pair ("y" := (7 ||| "x")) (Pair "x" ("x" := (1 ||| 2)))
+test9 = ok "test9" [(7,(1,1)),(7,(2,2)),(1,(1,1)),(2,(2,2))] $
+  Pair ("y" := (7 ||| "x")) (Pair "x" ("x" := (1 ||| 2)))
 
 -- x's value should not be delayed, because x's RHS has no depenedncies
--- test10 works [((1,7),1)]
-test10 = Pair ("x" := (Pair 1 7 |||
+test10 = ok "test10" [((1,7),1)] $
+         Pair ("x" := (Pair 1 7 |||
                        Pair "y" ("y" := 2)))
               (Fst "x" === 1)
 
 -- x's value depends on recursively bound z, so we get stuck.
 -- Produces []
--- LA: Is this the intended test.
-test11 = Pair ("x" := (Pair 1 "z" |||
+-- LA: Is this the intended test?
+test11 = ok "test11" ([]::[()]) $
+         Pair ("x" := (Pair 1 "z" |||
                         Pair "y" ("y" := 2)))
                (Pair ("x" === 1) ("z" := 3))
 
--- Works [(1,1)]
-test12 = "t" := Pair 1 (Fst "t")
+test12 = ok "test12" [(1,1)] $
+  "t" := Pair 1 (Fst "t")
 
--- Works [(1,1)]
-test13 = "x" := 1 ||| 2 `semi` "y" := ("x" === 1) `semi` ("x" # "y")
+test13 = ok "test13" [(1,1)] $
+  "x" := 1 ||| 2 `semi` "y" := ("x" === 1) `semi` ("x" # "y")
 
 -- Fails (equalLenient)
-test14 = "y" := ("x" === 1) `semi` "x" := 1 ||| 2 `semi` ("x" # "y")
+test14 = bad "test14" $
+  "y" := ("x" === 1) `semi` "x" := 1 ||| 2 `semi` ("x" # "y")
 
 -- Generates an error, as it should
-test15 = Error
+test15 = bad "test15"
+  Error
 
 -- Generates an error, as it should
-test16 = Error `semi` 1
+test16 = bad "test16" $
+  Error `semi` 1
 
 -- Generates an error, as it should
-test17 = (2 # Error) `semi` 1
+test17 = bad "test17" $
+   (2 # Error) `semi` 1
 
-test18 = For (1|||2|||3) 5
+-- Cascaded forward references
+test18 = ok "test18" [3,7,2,2] $
+         "x" := ("y" ||| 2)  `semi`
+         "y" := (3 ||| "z")  `semi`
+         "z" := 7            `semi`
+         "x"
 
-test19 = For ("x" := 1|||2|||3) "x"
+test19 = ok "test19" [(5,5,5)] $
+  For (1|||2|||3) 5
 
-test20 = For ("x" := 1|||2|||3 `semi` "y" := 4|||5) ("x" # "y")
+test20 = ok "test20" [(1,2,3)] $
+  For ("x" := 1|||2|||3) "x"
 
-test21 = "y" := 4|||5 `semi` For ("x" := 1|||2|||3) ("x" # "y")
+test21 = ok "test21" [((1,4),(1,5),(2,4),(2,5),(3,4),(3,5))] $
+  For ("x" := 1|||2|||3 `semi` "y" := 4|||5) ("x" # "y")
 
-test22 = For ("y" := 4|||5) $ For ("x" := 1|||2|||3) ("x" # "y")
+test22 = ok "test22" [((1,4),(2,4),(3,4)),
+                      ((1,5),(2,5),(3,5))] $
+  "y" := 4|||5 `semi` For ("x" := 1|||2|||3) ("x" # "y")
 
-test23 = For ("x" := 1|||2|||3) ("x" ||| 99)
+test23 = ok "test23" [(((1,4),(2,4),(3,4)),
+                       ((1,5),(2,5),(3,5)))] $
+  For ("y" := 4|||5) $ For ("x" := 1|||2|||3) ("x" # "y")
 
-test24 = For ("x" := 1|||2|||"y" `semi` "y" := "z" `semi` "z" := 3) "x"
+test24 = ok "test24" [(1,2,3),(1,2,99),(1,99,3),(1,99,99),(99,2,3),(99,2,99),(99,99,3),(99,99,99)] $
+  For ("x" := 1|||2|||3) ("x" ||| 99)
 
-test25 = For ("x" := 1|||2|||3) ("y" `wher` "y" := "x" + 1)
+test25 = ok "test25" [(1,2,3)] $
+  For ("x" := 1|||2|||"y" `semi` "y" := "z" `semi` "z" := 3) "x"
 
--- This doesn't work properly.
-test26 = "x" := (1 # "y") `semi` "y" := (2 # "z") `semi` "z" := 3 `semi` "x"
+test26 = ok "test26" [(2,3,4)] $
+  For ("x" := 1|||2|||3) ("y" `wher` "y" := "x" + 1)
+
+test27 = ok "test27" [(1,(2,3))] $
+  "x" := (1 # "y") `semi` "y" := (2 # "z") `semi` "z" := 3 `semi` "x"
+
+
+testAll = mapM_ testEx
+  [test1,test2,test3,test4,test5,test6,test7,test8,test9,test10,
+   test11,test12,test13,test14,test15,test16,test17,test18,
+   test19,test20,test21,test22,test23,test24,test25,test26,test27
+  ]
+
