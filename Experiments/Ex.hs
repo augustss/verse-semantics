09eabac0cf9179cdc51e4c1ae7a98738ca9ff9d7
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-01-24 19:30:41 +0100

    Be less extension happy.

--- Experiments/Ex.hs
+++ Experiments/Ex.hs
@@ -10,9 +10,7 @@
 --   Ex "good" (Just 3) (1+2)
 --   Ex "bad" Nothing (error "boo")
 --   Ex "worse" Nothing (let loop () = loop () in loop ())
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
 module Ex(Ex(..), testEx) where
 import Control.DeepSeq
 import Control.Exception
@@ -22,15 +20,15 @@ limit :: Int
 limit = 100000  -- us that the test is allowed to take before timeout
 
 data Ex a = Ex
-  { name :: String,
-    ref :: Maybe a,
-    test :: a
+  { name :: String,    -- test name
+    ref :: Maybe a,    -- exoected result, or Nothing if it's expected to fail
+    test :: a          -- lazy value to compute
   }
   deriving (Show)
 
 testEx :: (Eq a, NFData a, Show a) => Ex a -> IO ()
-testEx Ex{..} = do
-  let exio = evaluate $ force test
+testEx ex = do
+  let exio = evaluate $ force $ test ex
   res <- catch (timeout limit exio) (\ (_ :: SomeException) -> return Nothing)
-  putStrLn $ name ++ " " ++ if res == ref then "OK" else "failed: " ++ show res
+  putStrLn $ name ex ++ " " ++ if res == ref ex then "OK" else "failed: " ++ show res
 
