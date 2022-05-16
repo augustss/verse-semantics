a1766d815df3a5f566485c973024103185a8f905
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-05-16 14:52:09 +0200

    Generalize main loop.

--- Parser/Main.hs
+++ Parser/Main.hs
@@ -24,8 +24,32 @@ main :: IO ()
 main = runCommand command
 
 data CState = CState
-  { lastExpr :: Maybe Expr
+  { lastExpr :: SomeExpr
   }
+
+data SomeExpr = NoExpr | Parsed Expr | Desugared Expr | Cored Core
+
+asExpr :: SomeExpr -> Expr
+asExpr NoExpr = error "No current expression"
+asExpr (Parsed e) = e
+asExpr (Desugared e) = e
+asExpr (Cored _) = error "Current expression is Core"
+
+asDesugared :: SomeExpr -> Expr
+asDesugared (Parsed e) = desugar e
+asDesugared e = asExpr e
+
+instance Show SomeExpr where
+  show NoExpr = "No current expression"
+  show (Parsed e) = show e
+  show (Desugared e) = show e
+  show (Cored e) = show e
+
+instance Pretty SomeExpr where
+  pPrintPrec _ _ NoExpr = text "No current expression"
+  pPrintPrec l p (Parsed e) = pPrintPrec l p e
+  pPrintPrec l p (Desugared e) = pPrintPrec l p e
+  pPrintPrec l p (Cored e) = pPrintPrec l p e
 
 command :: Command CState
 command = Command
@@ -42,16 +66,16 @@ command = Command
   , c_greet = "Verse parse&desugar testing.\nUse :help for help, and :quit to quit."
   , c_bye = "Bye!"
   , c_prompt = "> "
-  , c_state = CState { lastExpr = Nothing }
+  , c_state = CState { lastExpr = NoExpr }
   , c_history = Just ".versei"
   }
 
-updateLastExpr :: CState -> Expr -> IO CState
-updateLastExpr s e = pure s{ lastExpr = Just e }
+updateLastExpr :: CState -> SomeExpr -> IO CState
+updateLastExpr s e = pure s{ lastExpr = e }
 
 cRead :: Run CState
 cRead fn s =
-  tryIt (pure s) (updateLastExpr s) $ do
+  tryIt (pure s) (updateLastExpr s . Parsed) $ do
     file <- readFile fn
     let prog = parseDie pFile fn file
     when (prog == prog) $
@@ -60,19 +84,17 @@ cRead fn s =
 
 cParseLine :: Run CState
 cParseLine line s =
-  tryIt (pure s) (updateLastExpr s) $ do
+  tryIt (pure s) (updateLastExpr s . Parsed) $ do
     let prog = parseDie pFile "<interactive>" line
     pp prog
     pure prog
 
-withLastExpr :: (Expr -> CState -> IO CState) -> Run CState
+withLastExpr :: (SomeExpr -> CState -> IO CState) -> Run CState
 withLastExpr cmd line s = do
   s' <- if null line then pure s else cParseLine line s
-  case lastExpr s' of
-    Nothing -> do putStrLn "No last expression"; pure s'
-    Just e -> cmd e s'
+  cmd (lastExpr s') s'
 
-cTransform :: (Expr -> Expr) -> Run CState
+cTransform :: (SomeExpr -> SomeExpr) -> Run CState
 cTransform tr =
   withLastExpr $ \ e s ->
     tryIt (pure s) (updateLastExpr s) $ do
@@ -81,18 +103,13 @@ cTransform tr =
       pure e'
 
 cDesugar :: Run CState
-cDesugar = cTransform desugar
+cDesugar = cTransform (Desugared . desugar . asExpr)
 
 cSimplify :: Run CState
-cSimplify = cTransform simplify
+cSimplify = cTransform (Desugared . simplify . asExpr)
 
 cCore :: Run CState
-cCore =
-  withLastExpr $ \ e s ->
-    tryIt (pure s) (updateLastExpr s) $ do
-      let e' = exprToCore e
-      pp e'
-      pure e
+cCore = cTransform (Cored . exprToCore . asDesugared)
 
 cShow :: Run CState
 cShow =
