9f57be9ee2564ce484aefe736b2d4ae2a6073e83
Koen Claessen <koen@chalmers.se>
2022-06-02 10:18:36 +0200

    parentheses

---
+++ TRS/Show.hs
@@ -0,0 +1,13 @@
+module Show where
+
+--------------------------------------------------------------------------------
+
+class Show a => Parens a where
+  parens :: a -> Bool
+
+show' :: Parens a => a -> String
+show' e | parens e  = "(" ++ show e ++ ")"
+        | otherwise = show e
+
+--------------------------------------------------------------------------------
+
