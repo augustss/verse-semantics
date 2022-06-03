3278cf080acf85d6ee6eeffb5d4d8a7002a36934
Koen Claessen <koen@chalmers.se>
2022-06-03 13:43:42 +0200

    added a simple property for the rules to be tested with QuickCheck

--- TRS/Main.hs
+++ TRS/Main.hs
@@ -4,6 +4,7 @@ import Core
 import Rules
 import TRS
 import Bind
+import Test.QuickCheck
 
 --------------------------------------------------------------------------------
 
@@ -16,7 +17,13 @@ ex2 = ARR [] :=: (VAR x :=: INT 3)
 
 --------------------------------------------------------------------------------
 
-main = sequence_ [ print t | t <- normalForms rules ex2 ]
+main = quickCheck prop_NormalForms
+
+prop_NormalForms p =
+  case normalFormsFuel 999 rules p of
+    []          -> whenFail (print "DOES NOT TERMINATE") False
+    q1 : q2 : _ -> whenFail (print q1 >> print q2) False
+    _           -> property True
 
 --------------------------------------------------------------------------------
 
