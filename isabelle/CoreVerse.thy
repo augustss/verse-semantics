cc9e1dbed20b7dcc0a5f1634442ec1899252bc41
Joachim Breitner <mail@joachim-breitner.de>
2022-07-06 14:59:36 +0200

    contexts

--- isabelle/CoreVerse.thy
+++ isabelle/CoreVerse.thy
@@ -93,159 +93,123 @@ nominal_inductive is_exp avoids is_exp_Def: x | is_val_Lam: x
   by (auto simp add: fresh_star_def)
 
 
-inductive rule_Seq where
-  rule_Seq: "rule_Seq (Seq v e) e"
-equivariance rule_Seq
-
-
-
-inductive cc for R where
-  here: "R x y \<Longrightarrow> cc R x y"
-| under_Seql: "cc R x y \<Longrightarrow> cc R (Seq x e) (Seq y e)"
-| under_Seqr: "cc R x y \<Longrightarrow> cc R (Seq e x) (Seq e y)"
-| under_Barl: "cc R x y \<Longrightarrow> cc R (Bar x e) (Bar y e)"
-| under_Barr: "cc R x y \<Longrightarrow> cc R (Bar e x) (Bar e y)"
-| under_Appl: "cc R x y \<Longrightarrow> cc R (App x e) (App y e)"
-| under_Appr: "cc R x y \<Longrightarrow> cc R (App e x) (App e y)"
-| under_Def: "cc R x y \<Longrightarrow> cc R (Def z x) (Def z y)"
-| under_One: "cc R x y \<Longrightarrow> cc R (One x) (One y)"
-| under_All: "cc R x y \<Longrightarrow> cc R (All x) (All y)"
-| under_Tupl: "cc R x y \<Longrightarrow> cc R (Tup x e) (Tup y e)"
-| under_Tupr: "cc R x y \<Longrightarrow> cc R (Tup e x) (Tup e y)"
-| under_Lam: "cc R x y \<Longrightarrow> cc R (Lam z x) (Lam z y)"
-equivariance cc
-
-inductive_cases cc_SeqE[elim!, case_names]: "cc R (Seq e1 e2) e3"
-
 inductive_cases is_exp_SeqE[elim!]: "is_exp (Seq e1 e2)"
 inductive_cases is_val_SeqE[elim!]: "is_val (Seq e1 e2)"
 
 
-definition "VR = cc (rule_Seq)"
+(* Contexts *)
+
+datatype C =
+  CHole
+| CSeql C exp
+| CSeqr exp C
+| CBarl C exp
+| CBarr exp C
+| CAppl C exp
+| CAppr exp C
+| CDef var C
+| COne C
+| CAll C
+| CTupl C exp
+| CTupr exp C
+| CLam var C
+
+function appC :: "C \<Rightarrow> exp \<Rightarrow> exp" where
+  "appC CHole e' = e'"
+| "appC (CSeql C e) e' = Seq (appC C e') e"
+| "appC (CSeqr e C) e' = Seq e (appC C e')"
+| "appC (CBarl C e) e' = Bar (appC C e') e"
+| "appC (CBarr e C) e' = Bar e (appC C e')"
+| "appC (CAppl C e) e' = App (appC C e') e"
+| "appC (CAppr e C) e' = App e (appC C e')"
+| "appC (CDef x C) e' = Def x (appC C e')"
+| "appC (COne C) e' = One (appC C e')"
+| "appC (CAll C) e' = All (appC C e')"
+| "appC (CTupl C e) e' = Tup (appC C e') e"
+| "appC (CTupr e C) e' = Tup e (appC C e')"
+| "appC (CLam x C) e' = Lam x (appC C e')"
+by pat_completeness auto+
+termination by lexicographic_order
+(* TODO: Not equivariant! equivariance appC *)
+
+
+function compC :: "C \<Rightarrow> C \<Rightarrow> C" where
+  "compC CHole e' = e'"
+| "compC (CSeql C e) e' = CSeql (compC C e') e"
+| "compC (CSeqr e C) e' = CSeqr e (compC C e')"
+| "compC (CBarl C e) e' = CBarl (compC C e') e"
+| "compC (CBarr e C) e' = CBarr e (compC C e')"
+| "compC (CAppl C e) e' = CAppl (compC C e') e"
+| "compC (CAppr e C) e' = CAppr e (compC C e')"
+| "compC (CDef x C) e' = CDef x (compC C e')"
+| "compC (COne C) e' = COne (compC C e')"
+| "compC (CAll C) e' = CAll (compC C e')"
+| "compC (CTupl C e) e' = CTupl (compC C e') e"
+| "compC (CTupr e C) e' = CTupr e (compC C e')"
+| "compC (CLam x C) e' = CLam x (compC C e')"
+by pat_completeness auto+
+termination by lexicographic_order
+
+lemma appC_appC_compC:
+"appC C1 (appC C2 e) = appC (compC C1 C2) e"
+by (induction C1) auto
+
+type_synonym red = "exp \<Rightarrow> exp \<Rightarrow> bool"
+
+inductive congruent :: "red \<Rightarrow> bool"  where
+  congruentI: "(\<And> x y C. R x y \<Longrightarrow> R (appC C x) (appC C y)) \<Longrightarrow> congruent R"
+
+lemma congruentE[elim, consumes 2]:
+  assumes "congruent R" and "R x y"
+  shows "R (appC C x) (appC C y)"
+  using assms
+  by (simp add: congruent.simps)
+
+lemma congruent_star:
+  assumes "congruent R"
+  shows "congruent R\<^sup>*\<^sup>*"
+proof
+  fix x y C
+  assume "R\<^sup>*\<^sup>* x y"
+  then show "R\<^sup>*\<^sup>* (appC C x) (appC C y)"
+  proof (induction rule: converse_rtranclp_induct)
+    case base
+    then show ?case..
+  next
+    case (step x z)
+    from `R x z`
+    have "R (appC C x) (appC C z)" using  `congruent R` by auto
+    with `R\<^sup>*\<^sup>* (appC C z) (appC C y)`
+    show ?case by auto
+  qed
+qed
+
+inductive underC :: "C \<Rightarrow> red \<Rightarrow> red" for C and R where
+  underCI: "R x y \<Longrightarrow> underC C R (appC C x) (appC C y)"
+
+
+inductive cc :: "red \<Rightarrow> red" for R where
+  ccI: "R x y \<Longrightarrow> cc R (appC C x) (appC C y)"
+
+lemma congruent_cc[simp]:
+  "congruent (cc R)"
+  by (auto intro!: congruentI elim!:cc.cases underC.cases
+           simp add: appC_appC_compC intro: underC.intros cc.intros)
+
 
 lemma cc_local_confluence:
-  assumes "\<And> e1 e2 e3.
-   R e1 e2 \<Longrightarrow>
-   is_exp e1 \<Longrightarrow> is_exp e2 \<Longrightarrow> is_exp e3 \<Longrightarrow>
-   cc R e1 e3 \<Longrightarrow>
-  ((cc R)\<^sup>*\<^sup>* OO ((cc R)\<inverse>\<inverse>)\<^sup>*\<^sup>*) e2 e3"
-  shows "
-   is_exp e1 \<Longrightarrow> is_exp e2 \<Longrightarrow> is_exp e3 \<Longrightarrow>
-   cc R e1 e2 \<Longrightarrow> cc R e1 e3 \<Longrightarrow>
-  ((cc R)\<^sup>*\<^sup>* OO ((cc R)\<inverse>\<inverse>)\<^sup>*\<^sup>*) e2 e3"
-proof (induction e1 arbitrary: e2 e3 rule:exp.induct)
-  case (Seq x1 x2)
+  assumes "congruent S"
+  assumes "symp R"
+  assumes "R\<inverse>\<inverse> OO R \<le> R'"
+  assumes "\<And> C. R\<inverse>\<inverse> OO underC C R \<le> R'"
+  shows "cc R\<inverse>\<inverse> OO cc R \<le> S"
+  sorry
 
-  from `cc R (Seq x1 x2) e2`
-  show ?case
-  apply rule
-  proof(goal_cases)
-    case 1
-    then show ?case by (rule assms) (auto intro: Seq)
-  next
-    case (2 x1')
-    then show ?case sorry
-  next
-    case (3 y)
-    then show ?case sorry
-  qed
-    case goal1
-    apply rule
-      apply (erule assms;fact)
-     apply (rule Seq.IH(1))
-    using Seq apply fast
-    using Seq apply fast
-    using Seq apply fast
+inductive rule_Seq where
+  rule_Seq: "rule_Seq (Seq v e) e"
+equivariance rule_Seq
+definition "VR = cc (rule_Seq)"
 
-         apply fact
-
-    using Seq
-    apply auto
-    apply (subst e2)
-    apply (rule Seq.IH)
-    
-next
-  case (Bar x1 x2)
-  then show ?case sorry
-next
-  case (App x1 x2)
-  then show ?case sorry
-next
-  case (Def x1 x2)
-  then show ?case sorry
-next
-  case (One x)
-  then show ?case sorry
-next
-  case (All x)
-  then show ?case sorry
-next
-  case (Var x)
-  then show ?case sorry
-next
-  case (Const x)
-  then show ?case sorry
-next
-  case Unit
-  then show ?case sorry
-next
-  case (Tup x1 x2)
-  then show ?case sorry
-next
-  case (Lam x1 x2)
-  then show ?case sorry
-qed
-  
-
-theorem local_confluence: 
-  assumes "is_exp e1" "is_exp e2" "is_exp e3"
-  assumes "R e1 e2" "R e1 e3"
-  shows "(R\<^sup>*\<^sup>* OO (R\<inverse>\<inverse>)\<^sup>*\<^sup>*) e2 e3"
-proof-
-  show ?thesis
-  using assms
-  proof (induction e1 rule:exp.induct)
-    case (Seq x1 x2)
-    from `R (Seq x1 x2) e2`
-    show ?case unfolding R_def
-    proof
-      
-      assume "rule_Seq (Seq x1 x2) e2"
-      thus ?case
-    
-    qed
-      
-  next
-    case (Bar x1 x2)
-    then show ?case sorry
-  next
-    case (App x1 x2)
-    then show ?case sorry
-  next
-    case (Def x1 x2)
-    then show ?case sorry
-  next
-    case (One x)
-    then show ?case sorry
-  next
-    case (All x)
-    then show ?case sorry
-  next
-    case (Var x)
-    then show ?case sorry
-  next
-    case (Const x)
-    then show ?case sorry
-  next
-    case Unit
-    then show ?case sorry
-  next
-    case (Tup x1 x2)
-    then show ?case sorry
-  next
-    case (Lam x1 x2)
-    then show ?case sorry
-  qed
 
 
 
