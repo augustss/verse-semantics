6cda9f1f8480194d5cacd2ccaf352ef8240f3d14
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-09-29 18:53:35 +0200

    Add parser for Tim's test file.

---
+++ Parser/TimParse.hs
@@ -0,0 +1,26 @@
+module TimParse where
+import System.Environment
+
+import Parse
+import Expr
+
+data TimTest = TimTest Ident Expr
+
+pFileTimTest :: P [TimTest]
+pFileTimTest = skip *> many pTimTest <* eof
+
+pTimTest :: P TimTest
+pTimTest = TimTest <$> (pKeyword "test" *> pParens pIdent) <*> pBraces pExprSeq
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fn =
+        case args of
+          [] -> "VerseTestFiles/TimVerse-tests.verse"
+          [s] -> s
+          _ -> error "Use 0 or 1 argument"
+  file <- readFile fn
+  let tests = parseDie pFileTimTest fn file
+  putStrLn $ "Read " ++ fn ++ ", parsed tests: " ++ show (length tests)
+  
