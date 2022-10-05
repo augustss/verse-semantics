2a4ec642bcc5215d9df4f42ed5478dd0cc2d6361
Lennart Augustsson <lennart.augustsson@epicgames.com>
2022-10-05 12:08:16 +0200

    Handle a few more values.

--- Parser/DenSem.hs
+++ Parser/DenSem.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PatternSynonyms #-}
 module DenSem where
 import Expr(Ident(..))
@@ -50,14 +51,29 @@ data W
   deriving (Eq)
 
 allW :: [W]
-allW = [WInt i | i <- [0 .. 3]]
+allW = ints ++ tuples ++ fcns ++ [Wrong]
+  where
+    ints = [WInt i | i <- [0 .. 3]]
+    tuples = [WTuple []] ++ [WTuple [w1,w2] | w1 <- ints, w2 <- ints]
+    fcns = map WFunction $ [wAdd, wGt, wId, wInc, wGt0] ++
+                           [ func (const (unit i)) | i <- ints ]
+    wId = func $ unit
+    wInc = func $ \case WInt x -> unit (WInt (x+1)); _ -> empty
+    wGt0 = func $ \case WInt x | x > 0 -> unit (WInt x); _ -> empty
 
 newtype Func a b = Func (a -> b)
 func :: (a -> b) -> Func a b
 func f = Func f
 apFunc :: Func a b -> a -> b
 apFunc (Func f) a = f a
-instance Eq (Func a b) where (==) = undefined
+
+instance Eq (Func W (S W)) where
+  f == g = and [eqs (apFunc f w) (apFunc g w) | w <- allW ]
+    where
+      eqs ws1 ws2 | length ws1 == length ws2 = and $ zipWith eq ws1 ws2
+                  | otherwise = False
+      eq WFunction{} WFunction{} = True  -- pretend all function are equal
+      eq w1 w2 = w1 == w2
 
 type Env = Ident -> W
 ext :: Env -> Ident -> W -> Env
@@ -88,14 +104,19 @@ evalV r (Var i) = r i
 evalV _ (HNF (HInt i)) = WInt i
 evalV r (VLam x e) = WFunction $ func $ \ w -> evalE (ext r x w) e
 evalV r (VArray vs) = WTuple $ map (evalV r) vs
-evalV _ (VPrim "in'+'") = WFunction $ func f
-  where f (WTuple [WInt x, WInt y]) = unit $ WInt $ x+y
-        f _ = unit Wrong
-evalV _ (VPrim "in'>'") = WFunction $ func f
-  where f (WTuple [WInt x, WInt y]) | x > y = unit $ WInt x
-                                    | otherwise = empty
-        f _ = unit Wrong
+evalV _ (VPrim "in'+'") = WFunction wAdd
+evalV _ (VPrim "in'>'") = WFunction wGt
 evalV _ _ = undefined
+
+wAdd :: Func W (S W)
+wAdd = func f
+  where f (WTuple [WInt x, WInt y]) = unit $ WInt $ x+y
+        f _ = empty
+
+wGt :: Func W (S W)
+wGt = func f
+  where f (WTuple [WInt x, WInt y]) | x > y = unit $ WInt x
+        f _ = empty
 
 apply :: W -> W -> S W
 apply WInt{} _ = unit Wrong
