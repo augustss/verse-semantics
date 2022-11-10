bd8f971538458bbde622d271b3e14d1b84c61ac6
Koen Claessen <koen@chalmers.se>
2022-11-10 14:55:32 +0100

    refactor and have less nodes point to Nothing

--- TRS/TRSGraph.hs
+++ TRS/TRSGraph.hs
@@ -13,36 +13,27 @@ trsGraphFuelTrace env fuel rule x = M.fromListWith (++) (go S.empty fuel [start 
   go seen _ [] =
     []
 
-  go seen fuel (tx:txs) =
-    -- what does x point to?
-    [ (Just tx,
-        -- all terms it can reach in one step...
-        [ Just ty
-        | ty <- tys
-        , fuel /= 0 || ty `S.member` seen
-        ] ++
-        -- ...plus Nothing if we're out of fuel and still new terms are generated
-        [ Nothing
-        | fuel == 0 && new
-        ])
+  go seen 0 txs =
+    [ (Just tx, map Just (filter (`S.member` nodes) tys)
+             ++ [ Nothing | any (not . (`S.member` nodes)) tys ])
+    | (tx,tys) <- txys
     ] ++
-    -- add Nothing as a node if we're out of fuel and we still get new terms
     [ (Nothing, [])
-    | fuel == 0 && new
-    ] ++
-    -- take care of the rest of the terms in the stack
-    go (S.insert tx seen) ((fuel-1) `max` 0)
-       ( -- add the new terms only if we have fuel left
-         [ ty
-         | fuel /= 0
-         , ty <- tys
-         , not (ty `S.member` seen)
-         ]
-      ++ txs
-       )
+    | any (not . (`S.member` nodes)) (concatMap snd txys)
+    ]
    where
-    tys = [ y :<-- ((n,term tx):trace tx) | (n,y) <- step rule env (term tx) ]
+    nodes = seen `S.union` S.fromList txs
+    txys  = [ (tx, children tx) | tx <- txs ]
+
+  go seen fuel (tx:txs) =
+    (Just tx, map Just tys) :
+    go (S.insert tx seen) ((fuel-1) `max` 0)
+       ([ ty | ty <- tys, not (ty `S.member` seen) ] ++ txs)
+   where
+    tys = children tx
     new = any (not . (`S.member` seen)) tys
+
+  children tx = [ y :<-- ((n,term tx):trace tx) | (n,y) <- step rule env (term tx) ]
 
 normalFormsFuelTraceWithGraph :: (Show a, Ord a, Rec a)
                               => RuleEnv a -> Int -> Rule a -> a -> [[(String,a)]]
