0ef9b1c0b277226498b3a236b84e0ceb7ed947ff
Lennart Augustsson <augustss@CDW-0IA65KFGEVU.localdomain>
2022-11-29 16:00:11 +0100

    Fix equivalence test again.

--- HaskellRules/rules/Rules/Equiv.hs
+++ HaskellRules/rules/Rules/Equiv.hs
@@ -1,8 +1,6 @@
 module Rules.Equiv(equiv) where
-import Data.List(nub)
 import Rules.Core
-import TRS.TRS( step, normalFormsFuelTrace )
-import TRS.TRSGraph( normalFormsFuelTraceWithGraph )
+import TRS.TRS( step )
 import TRS.Tarjan
 import TRS.Traced
 import TRS.System
@@ -12,17 +10,8 @@ import TRS.System
 equiv :: TRSystem Expr -> Expr -> Expr -> Bool
 equiv sys e1 e2 = normalForm sys e1 == normalForm sys e2
 
-normalForms :: TRSystem Expr -> Expr -> [Traced Expr]
-normalForms sys
-  | rulesHaveStructural sys = normalFormsFuelTraceWithGraph defaultTRSFlags 99 (rules sys)
-  | otherwise               = normalFormsFuelTrace          defaultTRSFlags 99 (rules sys)
-
 normalForm :: TRSystem Expr -> Expr -> Expr
-normalForm sys e =
-  case nub $ map (norm sys) $ normalForms sys e of
-    [] -> error "normalForm: []"
-    [x] -> term x
-    xs -> error $ "normalForm: many\n" ++ show e ++ "\n" ++ show (map term xs)
+normalForm sys e = term $ norm sys $ start e
 
 norm :: TRSystem Expr -> Traced Expr -> Traced Expr
 norm sys = minimum . head . tarjan tstep
