ad15892b52d243f9db164fb40f5f408bdd0c6fe0
Andy Sonnenburg <andy22286@gmail.com>
2022-12-27 15:15:34 -0500

    Working logic mutable references.

--- VerseReferenceImpl/src/Control/Monad/Ref/Logic.hs
+++ VerseReferenceImpl/src/Control/Monad/Ref/Logic.hs
@@ -1,34 +1,56 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Monad.Ref.Logic
-  ( RefLogicT
+  ( module Control.Monad.Logic.Class
+  , RefLogicT
   , runRefLogicT
   ) where
 
 import Control.Applicative
-import Control.Monad (when)
+import Control.Monad.Error.Class
 import Control.Monad.Logic
+import Control.Monad.Logic.Class
 import Control.Monad.Ref
-import Control.Monad.Trans.Class
+import Control.Monad.State.Strict
 
 newtype RefLogicT m a = RefLogicT
-  { unRefLogicT :: LogicT (WordStateT m) a
+  { unRefLogicT :: LogicT (StateT (m ()) m) a
   } deriving ( Functor
              , Applicative
              , Monad
              , MonadFail
-             , MonadLogic
              )
 
-runRefLogicT :: Monad m => RefLogicT m a -> m [a]
-runRefLogicT = flip evalWordStateT minBound . observeAllT . unRefLogicT
+deriving instance MonadError e m => MonadError e (RefLogicT m)
 
-instance Monad m => Alternative (RefLogicT m) where
-  empty = RefLogicT empty
-  x <|> y = modify' (+ 1) *> RefLogicT (unRefLogicT x <|> unRefLogicT y)
+runRefLogicT :: Monad m => RefLogicT m a -> m [a]
+runRefLogicT = flip evalStateT (pure ()) . observeAllT . unRefLogicT
 
 instance MonadTrans RefLogicT where
   lift = RefLogicT . lift . lift
+
+instance Monad m => Alternative (RefLogicT m) where
+  empty = RefLogicT $ LogicT $ \ _ fk ->
+    put (pure ()) *> fk
+  x <|> y = RefLogicT $ LogicT $ \ sk fk ->
+    unLogicT (unRefLogicT x) sk $ unLogicT (unRefLogicT y) sk fk
+
+instance Monad m => MonadLogic (RefLogicT m) where
+  msplit m = RefLogicT $ LogicT $ \ sk fk -> do
+    (x, fk') <- runStateT' (msplit' m)
+    sk x $ lift fk' *> fk
+    where
+      runStateT' =
+        lift . flip runStateT (pure ())
+      msplit' m =
+        unLogicT (unRefLogicT m) sk' $ pure Nothing
+        where
+          sk' x fk = pure $ Just (x, RefLogicT (lift fk) >>= reflect)
 
 instance MonadRef m => MonadRef (RefLogicT m) where
   type Ref (RefLogicT m) = Ref m
@@ -37,55 +59,7 @@ instance MonadRef m => MonadRef (RefLogicT m) where
   writeRef ref x = RefLogicT $ LogicT $ \ sk fk -> do
     y <- readRef ref
     writeRef ref x
-    r <- get
     sk () $ do
-      r' <- get
-      writeRef ref y *> fk <* when (r /= r') (writeRef ref x)
-
-modify' :: Monad m => (Word -> Word) -> RefLogicT m ()
-modify' = RefLogicT . lift . modify
-
-newtype WordStateT m a = WordStateT
-  { runWordStateT :: Word -> m (S a)
-  }
-
-evalWordStateT :: Functor m => WordStateT m a -> Word -> m a
-evalWordStateT m s = extract <$> runWordStateT m s
-
-instance Functor m => Functor (WordStateT m) where
-  fmap f m = WordStateT $ fmap (fmap f) . runWordStateT m
-
-instance Monad m => Applicative (WordStateT m) where
-  pure x = WordStateT $ \ s -> pure $ S x s
-  f <*> x = WordStateT $ \ s -> do
-    S f s <- runWordStateT f s
-    S x s <- runWordStateT x s
-    pure $! S (f x) s
-
-instance Monad m => Monad (WordStateT m) where
-  x >>= f = WordStateT $ \ s -> do
-    S x s <- runWordStateT x s
-    runWordStateT (f x) s
-
-instance MonadTrans WordStateT where
-  lift m = WordStateT $ \ s -> flip S s <$> m
-
-instance MonadRef m => MonadRef (WordStateT m) where
-  type Ref (WordStateT m) = Ref m
-  newRef = lift . newRef
-  readRef = lift . readRef
-  writeRef ref = lift . writeRef ref
-
-get :: Applicative m => WordStateT m Word
-get = WordStateT $ \ s -> pure $! S s s
-
-modify :: Applicative m => (Word -> Word) -> WordStateT m ()
-modify f = WordStateT $ \ s -> pure $! S () (f s)
-
-data S a = S a !Word
-
-extract :: S a -> a
-extract (S x _) = x
-
-instance Functor S where
-  fmap f (S x y) = S (f x) y
+      writeRef ref y
+      modify (writeRef ref x *>)
+      fk
