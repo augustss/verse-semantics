797412dd22e70774bef5e3195d8f681b89f98af6
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-02-26 12:32:06 +0100

    New SEQ-SWAP-SIMON

--- HaskellRules/Rules/ICFP.hs
+++ HaskellRules/Rules/ICFP.hs
@@ -68,15 +68,15 @@ systemICFPE = s
 systemICFPP :: TRSystem Expr
 systemICFPP = s
   { sname = "ICFPP"
-  , description = description s ++ " - Simon's new SUBST and SEQ-SWAP"
-  , rules = rules s -= "SEQ-SWAP" -= "SEQ-SWAP-ORD" -= "SUBST" <> rulesSimonSwap <> rulesSimonSubst
+  , description = description s ++ ", Simon's SUBST"
+  , rules = rules s -= "SUBST" <> rulesSimonSubst
   }
-  where s = systemICFP
+  where s = systemICFPJ
 
 systemICFPJ :: TRSystem Expr
 systemICFPJ = s
   { sname = "ICFPJ"
-  , description = description s ++ " - Simon's SEQ-SWAP"
+  , description = description s ++ ", Simon's SEQ-SWAP"
   , rules = rules s -= "SEQ-SWAP" -= "SEQ-SWAP-ORD" <> rulesSimonSwap
   }
   where s = systemICFP
@@ -545,6 +545,10 @@ rulesSimonSwap env lhs =
   do e1 :>: (e2@(Var _x :=: Val _) :>: e3) <- [lhs]
 --     guard (case e1 of Var y :=: Val _ -> not (ltExpr env (Var y) (Var x)); _ -> True)
 
+{-
+     -- This side condition is not confluent, see tricky:QC11
+     guard (ltExpr env e2 e1)
+     -- This side condition has the same problem
      guard $
        -- First, order by choice-free-ness;
        -- choice free goes first
@@ -556,7 +560,7 @@ rulesSimonSwap env lhs =
            case isEqn e1 of
              False  -> True              -- need to swap
              True   -> ltExpr env e2 e1  -- use ordering
-
+-}
      pure $ e2 :>: (e1 :>: e3)
 
 rulesSimonSubst :: ERule
