1ff01971d0b4e5f6e7cb2b78ac91b280f825edf2
Koen Claessen <koen@chalmers.se>
2023-03-15 09:37:34 +0100

    baby verifier based on FOL

--- HaskellRules/verifier/Main.hs
+++ HaskellRules/verifier/Main.hs
@@ -1,156 +1,128 @@
 module Main where
 
-import Data.List( intercalate )
-import Data.Char( isAlpha, toUpper )
-import qualified Data.Set as S
+import FOL
+--import Data.List( intercalate )
+--import Data.Char( isDigit, isAlpha, toUpper )
+--import qualified Data.Set as S
+--import qualified Data.Map as M
 import Rules.Core
 import TRS.Bind
 
 --------------------------------------------------------------------------------
+-- Logical Semantics of VC 
 
-showIdent :: Ident -> String
-showIdent (Name s) = s
-showIdent (Prim p) = "pr_" ++ show p ++ "_"
-
-showVar :: Ident -> String
-showVar (Name (c:s))
-  | isAlpha c    = toUpper c : s
-  | otherwise    = "V" ++ (c:s)
-showVar (Prim p) = "PR_" ++ show p ++ "_"
-
-data Term
-  = Vr Ident
-  | Ap Ident [Term]
- deriving ( Eq, Ord )
-
-instance Free Term where
-  free (Vr x)    = [x]
-  free (Ap _ ts) = free ts
-
-instance Show Term where
-  show (Vr v)    = showVar v
-  show (Ap c []) = showIdent c
-  show (Ap f ts) = showIdent f ++ "(" ++ intercalate "," (map show ts) ++ ")"
-
-data Form
-  = FALSE
-  | TRUE
-  | Pred Ident [Term]
-  | Not Form
-  | Form :&&: Form
-  | Form :||: Form
-  | Forall (Bind Form)
-  | Exists (Bind Form)
- deriving ( Eq, Ord )
-
-instance Free Form where
-  free (Pred _ ts) = free ts
-  free (Not p)     = free p
-  free (p :&&: q)  = free (p,q)
-  free (p :||: q)  = free (p,q)
-  free (Forall b)  = free b
-  free (Exists b)  = free b
-  free _           = []
-
-instance Show Form where
-  show FALSE       = "$false"
-  show TRUE        = "$true"
-  show (Pred r []) = showIdent r
-  show (Pred r [s,t]) | showIdent r == "=" = show s ++ " = " ++ show t
-  show (Pred r ts) = showIdent r ++ "(" ++ intercalate "," (map show ts) ++ ")"
-  show (Not p)     = "~" ++ show1 p
-  show (p :&&: q)  = showAnd [p,q]
-  show (p :||: q)  = showOr [p,q]
-  show (Forall b)  = "!" ++ showBind b
-  show (Exists b)  = "?" ++ showBind b
-
-showAnd ps = intercalate " & " (map show1 (flat ps))
+-- results e t:
+-- "compute all the ways the expression e can produce a value corresponding to the term t"
+results :: Expr -> Term -> [Form]
+results (Val v)             t = [t .=. term v]
+results Fail                t = []
+results (Op op :@: v)       t = [app op v t]
+results (e1 :|: e2)         t = results e1 t ++ results e2 t
+results ((v :=: e1) :>: e2) t = [q1 :&&: q2 | q1 <- results e1 (term v), q2 <- results e2 t]
+results (Exi (Bind x e))    t = [Exists (Bind x q) | q <- results e t]
+results (One e)             t = [ones (results e t) (fails e)]
  where
-  flat ((p :&&: q) : ps) = flat (p:q:ps)
-  flat (p:ps)            = p : flat ps
-  flat []                = []
-  
-showOr ps = intercalate " | " (map show1 (flat ps))
+  ones []     _      = FALSE
+  ones [q]    _      = q
+  ones (q:qs) (f:fs) = q :||: (f :&&: ones qs fs)
+
+results e _ = error ("results: " ++ show e)
+
+-- success e:
+-- "compute all the ways the expression e can produce some value"
+-- success e =~= map (Exists t .) (results e t), but without the extra quantifier
+success :: Expr -> [Form]
+success (Val v)             = [TRUE]
+success Fail                = []
+success (Op op :@: v)       = [appSuccess op v]
+success (e1 :|: e2)         = success e1 ++ success e2
+success ((v :=: e1) :>: e2) = [q1 :&&: q2 | q1 <- results e1 (term v), q2 <- success e2]
+success (Exi (Bind x e))    = [Exists (Bind x q) | q <- success e]
+success (One e)             = [ones (success e) (fails e)]
  where
-  flat ((p :||: q) : ps) = flat (p:q:ps)
-  flat (p:ps)            = p : flat ps
-  flat []                = []
-  
-showBind (Bind x p) = "[" ++ showVar x ++ "]: " ++ show1 p  
+  ones []     _      = FALSE
+  ones [q]    _      = q
+  ones (q:qs) (f:fs) = q :||: (f :&&: ones qs fs)
 
-show1 p
-  | isAtom p  = show p
-  | otherwise = "(" ++ show p ++ ")"
- where
-  isAtom FALSE      = True
-  isAtom TRUE       = True
-  isAtom (Not _)    = True
-  isAtom (Pred p _) = not (isOp p)
-  isAtom _          = False
-  
-  isOp v = showIdent v == "="
+success e = error ("success: " ++ show e)
 
-(.=.) :: Term -> Term -> Form
-s .=. t = Pred (ident "=") [s,t]
+-- fails e:
+-- "compute all the ways the expression e can fail"
+fails :: Expr -> [Form]
+fails e = map Not (success e)
 
----
-
+-- term v: (trivially) convert the value v into a first-order term t
 term :: Expr -> Term
 term (Var v)  = Vr v
 term (Int k)  = Ap (ident (show k)) []
 term (Arr vs) = Ap (ident "tup") (map term vs)
-term e = error ("term: " ++ show e)
+term e        = error ("term: " ++ show e)
 
-forms :: Expr -> [Term -> Form]
-forms (Val v)             = [\t -> t .=. term v]
-forms Fail                = []
-forms (Op op :@: v)       = [\t -> app op v t]
-forms (e1 :|: e2)         = forms e1 ++ forms e2
-forms ((v :=: e1) :>: e2) = [\t -> q1 (term v) :&&: q2 t | q1 <- forms e1, q2 <- forms e2]
-forms (e1 :>: e2)         = forms (Exi (Bind x ((Var x :=: e1) :>: e2)))
-                              where x = identNotIn (free (e1,e2))
-forms (Exi (Bind x e))    = [\t -> Exists (Bind x (q t)) | q <- forms e]
-forms (One e)             = [ones (forms e)]
- where
-  ones []     _ = FALSE
-  ones [q]    t = q t
-  ones (q:qs) t = qt :||: (Forall (Bind x (Not (q (Vr x)))) :&&: ones qs t)
-   where
-    qt = q t
-    x  = identNotIn (free qt)
-forms e = error ("forms: " ++ show e)
-
+-- primitive operators
+-- TODO: add something about types
 app :: Op -> Expr -> Term -> Form
-app Ge (Arr [a,b]) t =
-  Pred (ident "ge") [term a, term b] :&&: (t .=. term a)
+app op (Arr [a,b]) t | op `elem` [Ge,Le,Gt,Lt] =
+  appSuccess op (Arr [a,b]) :&&:
+  (t .=. term a)
+
+app Add (Arr [a,b]) t =
+  t .=. Ap (ident "+") [term a, term b]
 
 app op a _ =
   error ("app: " ++ show op ++ "@" ++ show a)
 
----
+appSuccess :: Op -> Expr -> Form
+appSuccess Ge (Arr [a,b]) =
+  Pred (ident "<=") [term b, term a]
+
+appSuccess Le (Arr [a,b]) =
+  Pred (ident "<=") [term a, term b]
+
+appSuccess Gt (Arr [a,b]) =
+  Not (Pred (ident "<=") [term a, term b])
+
+appSuccess Lt (Arr [a,b]) =
+  Not (Pred (ident "<=") [term b, term a])
+
+appSucces _ _ =
+  TRUE
+
+--------------------------------------------------------------------------------
+-- Running Some Examples
 
 main :: IO ()
 main =
-  do putStrLn "-- expr --"
-     print e
-     putStrLn "-- formula --"
-     let [q] = forms e
-     putStr $ unlines $
-       [ "fof(goal, conjecture,"
-       , "  " ++ show (Forall (Bind a (Exists (Bind t (q (Vr t))))))
-       , ")."
-       ]
+  do putStrLn "-- PROGRAM --"
+     print e2
+     putStrLn "-- FORMULA --"
+     let [q] = success e1
+         pr  = Forall $ Bind a $ q
+     putStrLn (show pr)
+     b <- prove pr
+     if b then
+       putStrLn "==> program does not fail"
+      else
+       putStrLn "==> program may fail"
  where
-  e  = One (Exi $ Bind x $ Exi $ Bind y $
-             (Var x :=: One ( (Var y :=: Var a :>: Var y) :|: Int 2 ))
-         :>: (Var y :=: Int 1)
-         :>: (Var x :=: Int 2)
-         :>: Var x)
+  e0  = One (Exi $ Bind x $
+              (Var x :=: Var a)
+          :>: Var x)
+ 
+ 
+  e1  = One (Exi $ Bind x $ Exi $ Bind y $
+              (Var x :=: One ( (Var y :=: Var a :>: Var y) :|: Int 2 ))
+          :>: (Var y :=: Int 1)
+          :>: (Var x :=: Int 2)
+          :>: Var x)
 
-  t  = ident "result"
+  e2  = One (Exi $ Bind x $
+              (Var x :=: One ( (Op Ge :@: Arr [Var a, Int 3]) :|: Int 2 ))
+          :>: (Op Ge :@: Arr [Var x, Var a])
+        )
+
   x  = ident "x"
   y  = ident "y"
-  a  = ident "a"
+  a  = ident "input"
 
 ifThenElse :: [Ident] -> Expr -> Expr -> Expr -> Expr
 ifThenElse xs c p q =
@@ -171,4 +143,5 @@ f frec x = ifThenElse
  where
   x' = identNotIn (free x)
 
+--------------------------------------------------------------------------------
 
