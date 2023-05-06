3621705d3a5a80b50753d3f17ff93951a035a53c
Andy Sonnenburg <andy22286@gmail.com>
2023-05-05 22:51:14 -0400

    Some WASM-related bug fixes.

--- VerseReferenceImpl/www/src/worker.js
+++ VerseReferenceImpl/www/src/worker.js
@@ -6,7 +6,11 @@ const imports = { wasi_snapshot_preview1: wasi.wasiImport };
 
 const wasm = await WebAssembly.instantiateStreaming(fetch('./versewasm.wizer.wasm'), imports);
 
-const exports = wasm.instance.exports;
+const instance = wasm.instance;
+
+wasi.inst = instance;
+
+const exports = instance.exports;
 
 const memory = exports.memory;
 
@@ -19,16 +23,17 @@ const outPtrPtr = exports.calloc_ptr();
 self.onmessage = ({ data: { stdinString } }) => {
     const stdinLength = Buffer.byteLength(stdinString);
     const stdinPtr = exports.malloc(stdinLength);
+    let outLength;
     try {
         const stdinArray = new Uint8Array(memory.buffer, stdinPtr, stdinLength);
         encoder.encodeInto(stdinString, stdinArray);
-        const outLength = exports.verse_eval(stdinPtr, stdinLength, outPtrPtr);
-        const outPtrArray = new Uint32Array(memory.buffer, outPtrPtr, 1);
-        const outPtr = outPtrArray[0];
-        const outArray = new Uint8Array(memory.buffer, outPtr, outLength);
-        const outString = decoder.decode(outArray);
-        self.postMessage({ outString });
+        outLength = exports.verse_eval(stdinPtr, stdinLength, outPtrPtr);
     } finally {
         exports.free(stdinPtr);
     }
+    const outPtrArray = new Uint32Array(memory.buffer, outPtrPtr, 1);
+    const outPtr = outPtrArray[0];
+    const outArray = new Uint8Array(memory.buffer, outPtr, outLength);
+    const outString = decoder.decode(outArray);
+    self.postMessage({ outString });
 };
