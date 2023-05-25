b5f33732302137c003b7336e20019e3be6369cf8
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-05-24 17:13:50 -0700

    Tim's test works

--- HaskellRules/Rules/Verifier.hs
+++ HaskellRules/Rules/Verifier.hs
@@ -1,11 +1,7 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns -Wno-orphans #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Rules.Verifier  where
@@ -14,13 +10,6 @@ import TRS.TRS
 import Rules.Core hiding (Wrong)
 import Rules.ICFP (allSystemsICFP)
 import Control.Monad (guard)
--- import Epic.Print
--- import qualified Verifier.Verify as V
--- import Control.Monad (guard, filterM)
--- import Data.List (intersect)
--- import qualified Rules.ICFP as ICFP
--- import qualified Verifier.FOL as FOL
--- import Data.Maybe (maybeToList)
 
 trivVerifier :: TRSystem Expr
 trivVerifier = icfpVerifier
@@ -34,7 +23,6 @@ icfpVerifier = base'
   where
     base     = head allSystemsICFP
     base'    = base { rules = rules base }
-
 
 --------------------------------------------------------------------------------------
 -- | The "Context" in which a subsumption must hold; Tim's "G" -- set of "known facts"
@@ -97,60 +85,6 @@ proves :: QContext -> Expr -> Bool
 proves QEmp _       = False
 proves (QDef _ g) e = proves g e
 proves (QAsm p g) e = p == e || proves g e
-
-{-
- "A-APP" `TRSS.name`
-  do (ctx, g, (AFUN x t1 p t2) :@: AVAL t) <- execEX lhs
-     let freeT = free t
-     if x `notElem` freeT then
-        -- no name clash
-        pure (ctx (absBeta x t p t2), subsumes g t t1)
-     else do
-        -- The x has to be renamed to avoid capture
-        let freeE = free t1 ++ free p ++ free t2
-            x' = identNotIn (freeT ++ freeE)
-            (t1', p', t2') = substI x x' (t1, p, t2)
-        pure (ctx (absBeta x' t p' t2'), subsumes g t t1')
-  ++
-  "A-CONSEQ-R" `TRSS.name`
-  do (ctx, g, (Vval t) `Vis` t') <- execEX lhs
-     pure (ctx (aval t'), subsumes g (ABase t) t')
--}
-
-{-
-absBeta :: Ident -> AVal -> Form -> AVal -> Expr
-absBeta x t p t2 = EXI x ((Var x :=: aval t) :>: (Vasm p :>: aval t2))
-
-join :: (Form, AVal) -> (Form, AVal) -> Expr
-join (p1, t1)   (p2, t2)   = Vasm (joinForm p1 p2) :>: aval (joinAVal t1 t2)
-
-joinForm :: Form -> Form -> Form
-joinForm TRUE  _     = TRUE
-joinForm _     TRUE  = TRUE
-joinForm FALSE p     = p
-joinForm p     FALSE = p
-joinForm p1    p2    = if p1 == p2 then p1 else p1 :||: p2
-
-mergeForm :: Form -> Form -> Form
-mergeForm TRUE  p     = p
-mergeForm p     TRUE  = p
-mergeForm FALSE _     = FALSE
-mergeForm _     FALSE = FALSE
-mergeForm p1    p2    = p1 :||: p2
-
-joinAVal :: AVal -> AVal -> AVal
-joinAVal (ABase (Base (Bind a1 p1) ixs1)) (ABase (Base (Bind a2 p2) ixs2))
-  | a1 == a2 = aBase (Bind a1 (joinForm p1 p2)) (ixs1 `intersect` ixs2) -- TODO: name shift shenanigans
-joinAVal (AFun (Bind a1 (s1, p1, t1))) (AFun (Bind a2 (s2, p2, t2)))
-  | a1 == a2 = AFun (Bind a1 (joinAVal s1 s2, mergeForm p1 p2, joinAVal t1 t2))
-joinAVal  _ _ = error "todo: joinAVal"
-
-tLam :: Ident -> Expr -> Form -> Expr -> Expr
-tLam x e1 TRUE e2 = TLam (Bind x (e1, e2))
-tLam x e1 p    e2 = TLam (Bind x (e1, Vasm p :>: e2))
-
--}
-
 
 ----------------------------------------------------------------------
 -- | Expression Contexts
