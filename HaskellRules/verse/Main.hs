bc10d9649f8918571b69b5b9faec4c84aca756f6
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-06-22 14:46:27 +0200

    Do a beta reduction when inlining primitives for verification.
    This makes it more readable.

--- HaskellRules/verse/Main.hs
+++ HaskellRules/verse/Main.hs
@@ -269,11 +269,21 @@ cVerify = do
 replacePrim :: Core -> Core
 replacePrim = f
   where
-    f (CApply (CPrim i) v) | Just p <- lookup i verifyPrelude = CApply p (f v)
-    f (CPrim i) | Just p <- lookup i verifyPrelude = CValue p
+    f (CApply (CPrim i) v) | Just p <- lookup i verifyPrelude = cApply p (f v)
+    f (CApply (CLam a e) v) = cApply ([a], [e]) v
+    f (CPrim i) | Just p <- lookup i verifyPrelude = CValue $ toLam p
     f e = FrontEnd.Core.composOp f e
 
-verifyPrelude :: [(String, Value)]
+    arg = Ident noLoc "arg"
+    toLam (   [a],   es) = CLam a   $ CSeq es
+    toLam (as@[_,_], es) = CLam arg $ CDef as $ CSeq $ CUnify (CArray $ map CVar as) (CVar arg) : es
+    toLam _ = undefined  -- shouldn't happen
+
+    cApply ([a], e) v = subst a v $ CSeq e
+    cApply ([a1,a2], e) (CArray [v1,v2]) = subst a1 v1 $ subst a2 v2 $ CSeq e
+    cApply p v = CApply (toLam p) v
+
+verifyPrelude :: [(String, ([Ident], [Core]))]
 verifyPrelude =
   [ arithBinOpInt  "in'+'"
   , arithBinOpInt  "in'-'"
@@ -290,29 +300,27 @@ verifyPrelude =
   where
     arithBinOpInt  p = (p, arithBinOpInt' [] p)
     arithBinOpIntC p c = (p, arithBinOpInt' [c] p)
-    arithBinOpInt' c p = CLam xy $ CDef [x,y] $ CSeq $
-      [ CUnify (CArray [vx, vy]) (CVar xy), cInt vx, cInt vy] ++ c ++
-      [ cAssume (CDef [z] $ CSeq [CUnify vz (CApply (CPrim p) vxy), cInt vz, vz]) ]
+    arithBinOpInt' c p = ([x, y],
+      [ cInt vx, cInt vy] ++ c ++
+      [ cAssume $ CDef [z] $ CSeq [CUnify vz (CApply (CPrim p) (CArray [vx, vy])), cInt vz, vz] ])
 
     cmpBinOpInt  p = (p, cmpBinOpInt' p)
-    cmpBinOpInt' p = CLam xy $ CDef [x,y] $ CSeq
-      [ CUnify (CArray [vx, vy]) (CVar xy), cInt vx, cInt vy, CApply (CPrim p) vxy
-      , cAssume (CSeq [cInt vx, vx]) ]
+    cmpBinOpInt' p = ([x, y], 
+      [ cInt vx, cInt vy, CApply (CPrim p) (CArray [vx, vy]), cAssume (CSeq [cInt vx, vx]) ])
 
     yNe0 = CApply (CPrim "in'<>'") (CArray [vy, CInt 0])
 
-    arithUnOpInt p = (p, CLam x $ CSeq $
-      [ cInt vx, cAssume (CDef [z] $ CSeq [CUnify vz (CApply (CPrim p) vx), cInt vz, vz]) ])
+    arithUnOpInt p =
+      (p, ([x], [ cInt vx, cAssume (CDef [z] $ CSeq [CUnify vz (CApply (CPrim p) vx), cInt vz, vz]) ]))
 
     cInt e = CApply (CPrim "isInt$") e
-    xy = Ident noLoc "xy"
-    x = Ident noLoc "x"
-    y = Ident noLoc "y"
-    z = Ident noLoc "z"
-    vxy = CVar xy
+    x = Ident noLoc "$$x"
+    y = Ident noLoc "$$y"
+    z = Ident noLoc "$$z"
     vx = CVar x
     vy = CVar y
     vz = CVar z
+      
 
 cRules :: Run CState
 cRules "" s = do putStrLn $ "rules: " ++ sname (esystem s) ++ " - " ++ description (esystem s); pure s
