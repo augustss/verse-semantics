7344b6a9d87f1fd91fd1147c75c1ce7c7713740c
Koen Claessen <koen@chalmers.se>
2023-06-28 17:56:09 +0200

    aligning doc and implementation

--- HaskellRules/Rules/LeftToRight.hs
+++ HaskellRules/Rules/LeftToRight.hs
@@ -82,34 +82,23 @@ validE = ok
 
 allRules :: ERule
 allRules =
-     rulesFail
-  <> rulesSubst
-  <> rulesUni
-  <> rulesEqn
-  <> rulesExi
-  <> rulesAssoc
-  <> rulesElim
-  <> rulesApp
-  <> rulesOne
-  <> rulesAll
+     rulesApplication
+  <> rulesUnification
+  <> rulesSubstitution
+  <> rulesNormalization
   <> rulesChoice
+  <> rulesOneAndAll
 
 --------------------------------------------------------------------------------
 
-rulesFail :: ERule
-rulesFail _ lhs =
-  "FAIL-ELIM" `name`
-  do (_, Fail) <- choiceX lhs
-     pure Fail
- ++
-  "FAIL-CHOICE-R" `name`
-  do e1 :|: Fail <- [lhs]
-     pure e1
- ++
-  "FAIL-CHOICE-L" `name`
-  do Fail :|: e2 <- [lhs]
-     pure e2
+-- contexts
 
+-- V
+isVctx :: Ident -> Expr -> Bool
+isVctx x (Arr as) = Var x `elem` as || any (isVctx x) as
+isVctx _ _        = False
+
+-- X
 xX :: Expr -> [(Expr->Expr, Expr)]
 xX lhs =
   do pure (id, lhs)
@@ -126,185 +115,7 @@ xX lhs =
      (ctx, e) <- xX xe
      pure ((:>: e2). ctx, e)
 
---------------------------------------------------------------------------------
-
-rulesSubst :: ERule
-rulesSubst _ lhs =
-  "SUBST" `name`
-  do (Var x :=: Val v) :>: e <- [lhs]
-     guard (not (isVctx x v))
-     pure ((Var x :=: v) :>: subst [(x,v)] e)
- ++
-  "EQN-MOVE" `name`
-  do ce :>: ((Val v1 :=: Val v2) :>: e) <- [lhs]
-     guard (isChoiceFree ce)
-     pure ((v1 :=: v2) :>: (ce :>: e))
- ++
-  "EQN-SWAP" `name`
-  do (Val v :=: Var x) <- [lhs]
-     pure (Var x :=: v)
-
-isVctx :: Ident -> Expr -> Bool
-isVctx x (Arr as) = Var x `elem` as || any (isVctx x) as
-isVctx _ _        = False
-
---------------------------------------------------------------------------------
-
-rulesUni :: ERule
-rulesUni _ lhs =
-  "UNI-ARR" `name`
-  do (Arr as :=: Arr bs) :>: e <- [lhs]
-     guard (length as == length bs)
-     pure (foldr (:>:) e [ a :=: b | (a,b) <- as `zip` bs ])
- ++
-  "UNI-INT" `name`
-  do (Int k :=: Int k') :>: e <- [lhs]
-     guard (k == k')
-     pure e
- ++
-  "UNI-OCCURS" `name`
-  do (Var x :=: Val v) :>: _ <- [lhs]
-     guard (isVctx x v)
-     pure Fail
- ++
-  "UNI-FAIL" `name`
-  do (HNF a :=: HNF b) :>: _ <- [lhs]
-     guard (a =/= b)
-     pure Fail
- where
-  Int k  =/= Int k' = k /= k'
-  Arr as =/= Arr bs = length as /= length bs
-  _      =/= _      = True
-
---------------------------------------------------------------------------------
-
-rulesEqn :: ERule
-rulesEqn _ lhs =
-  "EQN-SEQ-ASSOC" `name`
-  do (v2 :=: ((v1 :=: e1) :>: e2)) :>: e3 <- [lhs]
-     pure ((v1 :=: e1) :>: (v2 :=: e2) :>: e3)
-
---------------------------------------------------------------------------------
-
-rulesExi :: ERule
-rulesExi _ lhs =
-  "EXI-FLOAT" `name`
-  do (ctx, Exi bnd) <- xX lhs
-     let Bind x e = alphaRename (free (ctx (Arr []))) bnd
-     pure (Exi (Bind x (ctx e)))
- ++
-  "EXI-SWAP" `name`
-  do Exi (Bind x (Exi (Bind y e))) <- [lhs]
-     pure (Exi (Bind y (Exi (Bind x e))))
-
---------------------------------------------------------------------------------
-
-rulesAssoc :: ERule
-rulesAssoc _ lhs =
-  "CHOICE-ASSOC" `name`
-  do (e1 :|: e2) :|: e3 <- [lhs]
-     pure (e1 :|: (e2 :|: e3))
-
---------------------------------------------------------------------------------
-
-rulesElim :: ERule
-rulesElim _ lhs =
-  "EXI-ELIM" `name`
-  do Exi (Bind x e) <- [lhs]
-     guard (x `notElem` free e)
-     pure e
--- ++
---  "EQN-ELIM" `name`
---  do Exi (Bind x ((Var x' :=: Val v) :>: e)) <- [lhs]
---     guard (x == x')
---     guard (x `notElem` free (v,e))
---     pure e
- ++
-  "EQN-ELIM" `name`
-  do Exi (Bind x xe) <- [lhs]
-     (ctx, (Var x' :=: Val v) :>: e) <- xX xe
-     guard (x == x')
-     guard (x `notElem` free (ctx (Val v :>: e)))
-     pure (ctx e)
-
---------------------------------------------------------------------------------
-
-rulesApp :: ERule
-rulesApp _ lhs =
-  "APP-LAM" `name`
-  do Lam bnd :@: Val v <- [lhs]
-     let Bind x e = alphaRename (free v) bnd
-     pure (Exi (Bind x ((Var x :=: v) :>: e)))
- ++
-  "APP-ARR" `name`
-  do Arr as :@: Val v <- [lhs]
-     pure (foldr (:|:) Fail [ (v :=: Int i) :>: a | (i,a) <- [0..] `zip` as ])
- ++
-  "APP-ADD" `name`
-  do Op Add :@: Arr [Int i, Int j] <- [lhs]
-     pure (Int (i+j))
- ++
-  "APP-GT" `name`
-  do Op Gt :@: Arr [Int i, Int j] <- [lhs]
-     pure (if i > j then Int i else Fail)
-
---------------------------------------------------------------------------------
-
-rulesOne :: ERule
-rulesOne _ lhs =
-  "ONE-FAIL" `name`
-  do One Fail <- [lhs]
-     pure Fail
- ++
-  "ONE-VAL" `name`
-  do One (Val v) <- [lhs]
-     pure v
- ++
-  "ONE-CHOICE" `name`
-  do One (Val v :|: _) <- [lhs]
-     pure v
-
-rulesAll :: ERule
-rulesAll _ lhs =
-  "ALL-FAIL" `name`
-  do All Fail <- [lhs]
-     pure (Arr [])
- ++
-  "ALL-CHOICE" `name`
-  do All e <- [lhs]
-     let as = choices e
-     guard (all isVal as)
-     pure (Arr as)
- where
-  choices (e1 :|: e2) = choices e1 ++ choices e2
-  choices e           = [e]
-
---------------------------------------------------------------------------------
-
-isChoiceFree :: Expr -> Bool
-isChoiceFree (One _)       = True
-isChoiceFree (All _)       = True
-isChoiceFree (Val _)       = True
-isChoiceFree (Op op :@: _) = True -- op `elem` [Add, Gt, ..]
-isChoiceFree (e1 :>: e2)   = isChoiceFree e1 && isChoiceFree e2
-isChoiceFree (e1 :=: e2)   = isChoiceFree e1 && isChoiceFree e2
-isChoiceFree _             = False
-
-rulesChoice :: ERule
-rulesChoice _ lhs =
-  "EQN-CHOICE" `name`
-  do (Val v :=: (e1 :|: e2)) :>: e3 <- [lhs]
-     pure (((v :=: e1) :>: e3) :|: ((v :=: e2) :>: e3))
- ++
-  "SEQ-CHOICE" `name`
-  do ce :>: (e1 :|: e2) <- [lhs]
-     guard (isChoiceFree ce)
-     pure ((ce :>: e1) :|: (ce :>: e2))
- ++
-  "EXI-CHOICE" `name`
-  do Exi (Bind x (e1 :|: e2)) <- [lhs]
-     pure (Exi (Bind x e1) :|: Exi (Bind x e2))
-
+-- CX
 choiceX :: Expr -> [(Expr->Expr, Expr)]
 choiceX lhs =
   do pure (id, lhs)
@@ -325,6 +136,165 @@ choiceX lhs =
   do Exi (Bind x xe) <- [lhs]
      (ctx, e) <- choiceX xe
      pure ((Exi . Bind x) . ctx, e)
+
+-- ce
+isChoiceFree :: Expr -> Bool
+isChoiceFree (One _)       = True
+isChoiceFree (All _)       = True
+isChoiceFree (Val _)       = True
+isChoiceFree (Op op :@: _) = True -- op `elem` [Add, Gt, ..]
+isChoiceFree (e1 :>: e2)   = isChoiceFree e1 && isChoiceFree e2
+isChoiceFree (e1 :=: e2)   = isChoiceFree e1 && isChoiceFree e2
+isChoiceFree _             = False
+
+--------------------------------------------------------------------------------
+
+rulesApplication :: ERule
+rulesApplication _ lhs =
+  "APP-ADD" `name`
+  do Op Add :@: Arr [Int i, Int j] <- [lhs]
+     pure (Int (i+j))
+ ++
+  "APP-GT" `name`
+  do Op Gt :@: Arr [Int i, Int j] <- [lhs]
+     pure (if i > j then Int i else Fail)
+ ++
+  "APP-LAM" `name`
+  do Lam bnd :@: Val v <- [lhs]
+     let Bind x e = alphaRename (free v) bnd
+     pure (Exi (Bind x ((Var x :=: v) :>: e)))
+ ++
+  "APP-TUP" `name`
+  do Arr as :@: Val v <- [lhs]
+     pure (foldr (:|:) Fail [ (v :=: Int i) :>: a | (i,a) <- [0..] `zip` as ])
+
+--------------------------------------------------------------------------------
+
+rulesUnification :: ERule
+rulesUnification _ lhs =
+  "U-LIT" `name`
+  do (Int k :=: Int k') :>: e <- [lhs]
+     guard (k == k')
+     pure e
+ ++
+  "U-TUP" `name`
+  do (Arr as :=: Arr bs) :>: e <- [lhs]
+     guard (length as == length bs)
+     pure (foldr (:>:) e [ a :=: b | (a,b) <- as `zip` bs ])
+ ++
+  "U-FAIL" `name`
+  do (HNF a :=: HNF b) :>: _ <- [lhs]
+     guard (a =/= b)
+     pure Fail
+ ++
+  "U-OCCURS" `name`
+  do (Var x :=: Val v) :>: _ <- [lhs]
+     guard (isVctx x v)
+     pure Fail
+ where
+  Int k  =/= Int k' = k /= k'
+  Arr as =/= Arr bs = length as /= length bs
+  _      =/= _      = True
+
+--------------------------------------------------------------------------------
+
+rulesSubstitution :: ERule
+rulesSubstitution _ lhs =
+  "SUBST" `name`
+  do (Var x :=: Val v) :>: e <- [lhs]
+     guard (not (isVctx x v))
+     pure ((Var x :=: v) :>: subst [(x,v)] e)
+ ++
+  "VAL-SWAP" `name`
+  do (Val v :=: Var x) <- [lhs]
+     pure (Var x :=: v)
+ ++
+  "EQN-FLOAT" `name`
+  do ce :>: ((Val v1 :=: Val v2) :>: e) <- [lhs]
+     guard (isChoiceFree ce)
+     pure ((v1 :=: v2) :>: (ce :>: e))
+
+--------------------------------------------------------------------------------
+
+rulesNormalization :: ERule
+rulesNormalization _ lhs =
+  "EXI-ELIM" `name`
+  do Exi (Bind x e) <- [lhs]
+     guard (x `notElem` free e)
+     pure e
+ ++
+  "EQN-ELIM" `name`
+  do Exi (Bind x xe) <- [lhs]
+     (ctx, (Var x' :=: Val v) :>: e) <- xX xe
+     guard (x == x')
+     guard (x `notElem` free (ctx (Val v :>: e)))
+     pure (ctx e)
+ ++
+  "EXI-FLOAT" `name`
+  do (ctx, Exi bnd) <- xX lhs
+     let Bind x e = alphaRename (free (ctx (Arr []))) bnd
+     pure (Exi (Bind x (ctx e)))
+ ++
+  "EXI-SWAP" `name`
+  do Exi (Bind x (Exi (Bind y e))) <- [lhs]
+     pure (Exi (Bind y (Exi (Bind x e))))
+ ++
+  "SEQ-ASSOC" `name`
+  do (v2 :=: ((v1 :=: e1) :>: e2)) :>: e3 <- [lhs]
+     pure ((v1 :=: e1) :>: (v2 :=: e2) :>: e3)
+
+--------------------------------------------------------------------------------
+
+rulesChoice :: ERule
+rulesChoice _ lhs =
+  "CHOICE-ASSOC" `name`
+  do (e1 :|: e2) :|: e3 <- [lhs]
+     pure (e1 :|: (e2 :|: e3))
+ ++
+  "CHOICE-FAIL-L" `name`
+  do Fail :|: e2 <- [lhs]
+     pure e2
+ ++
+  "CHOICE-FAIL-R" `name`
+  do e1 :|: Fail <- [lhs]
+     pure e1
+ ++
+  "FAIL" `name`
+  do (_, Fail) <- choiceX lhs
+     pure Fail
+ ++
+  "CHOICE" `name`
+  do (ctx, e1 :|: e2) <- choiceX lhs
+     pure (ctx e1 :|: ctx e2)
+
+--------------------------------------------------------------------------------
+
+rulesOneAndAll :: ERule
+rulesOneAndAll _ lhs =
+  "ONE-FAIL" `name`
+  do One Fail <- [lhs]
+     pure Fail
+ ++
+  "ONE-VAL" `name`
+  do One (Val v) <- [lhs]
+     pure v
+ ++
+  "ONE-CHOICE" `name`
+  do One (Val v :|: _) <- [lhs]
+     pure v
+ ++
+  "ALL-FAIL" `name`
+  do All Fail <- [lhs]
+     pure (Arr [])
+ ++
+  "ALL-CHOICE" `name`
+  do All e <- [lhs]
+     let as = choices e
+     guard (all isVal as)
+     pure (Arr as)
+ where
+  choices (e1 :|: e2) = choices e1 ++ choices e2
+  choices e           = [e]
 
 --------------------------------------------------------------------------------
 
