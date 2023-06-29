725c3743b5c666e3af89d07ed6fa5cc7bca1f383
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-06-29 11:50:26 +0200

    Make a version of tarjan that can return all normal forms.

--- HaskellRules/TRS/Tarjan.hs
+++ HaskellRules/TRS/Tarjan.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module TRS.Tarjan where
+module TRS.Tarjan(tarjan1, tarjan, tarjanAny) where
 
 import qualified Data.Map as M
 import Data.Map( (!) )
 import qualified Data.Set as S
 
-import Epic.List(takeUntil)
+import Epic.List(takeUntil, dropUntil)
 
 -----------------------------------------------------------------------------------
 
@@ -17,21 +17,35 @@ import Epic.List(takeUntil)
 -- https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
 
 type Fuel = Int
-type Kont a = Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> Maybe [a]
+type Kont a = Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> Maybe [[a]]
 
+-- Return one normal form.
 tarjan1 :: forall a . Ord a => Fuel -> (a -> [a]) -> a -> Maybe [a]
-tarjan1 afuel nexts x = strongc afuel 0 M.empty S.empty [] x (\ _ _ _ _ _ -> Nothing)
+tarjan1 afuel nexts a = hd <$> tarjanAny True afuel nexts a
+  where hd [x] = x
+        hd _ = undefined  -- This should never happen
+
+-- Return all normal forms.
+tarjan :: forall a . Ord a => Fuel -> (a -> [a]) -> a -> Maybe [[a]]
+tarjan = tarjanAny False
+
+-- If justOne is True then the returned list is always a singleton.
+tarjanAny :: forall a . Ord a => Bool -> Fuel -> (a -> [a]) -> a -> Maybe [[a]]
+tarjanAny justOne afuel nexts x = strongc afuel 0 M.empty S.empty [] x (\ _ _ _ _ _ -> Nothing)
  where
-  strongc :: Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> a -> Kont a -> Maybe [a]
+  strongc :: Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> a -> Kont a -> Maybe [[a]]
   strongc 0 _ _ _ _ _ _ = Nothing
   strongc fuel !index state onStack stack v k =
     visit (fuel-1) (index+1) (M.insert v (index,index) state) (S.insert v onStack) (v:stack) v (nexts v) k
   
-  visit :: Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> a -> [a] -> Kont a -> Maybe [a]
+  visit :: Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> a -> [a] -> Kont a -> Maybe [[a]]
   visit !fuel !index state onStack stack v [] k =
     if vindex == vlowlink then
       let xs = takeUntil (v==) stack in
-        Just xs -- <$> k fuel index state (foldr S.delete onStack xs) (dropUntil (v==) stack)
+        if justOne then
+          Just [xs]
+        else
+          (xs :) <$> k fuel index state (foldr S.delete onStack xs) (dropUntil (v==) stack)
     else
       k fuel index state onStack stack
    where
