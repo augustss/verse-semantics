889593c8304a5272710e32293fd82e3de4e5fd43
Andy Sonnenburg <andy22286@gmail.com>
2023-08-23 20:10:25 -0400

    Failing test.

--- VerseReferenceImpl/test/HUnit.hs
+++ VerseReferenceImpl/test/HUnit.hs
@@ -18,16 +18,22 @@ import Prelude hiding (all)
 import Test.HUnit
 
 main :: IO ()
-main = runTestTTAndExit test1
+main = runTestTTAndExit $ TestList
+  [ test1
+  , test2
+  ]
 
 data Val a
-  = Int {-# UNPACK #-} !Int deriving (Show, Eq, Functor, Foldable, Traversable)
+  = Int {-# UNPACK #-} !Int
+  | Tuple [a] deriving (Show, Eq, Functor, Foldable, Traversable)
 
 instance RowMatchable Val
 
 instance ZipMatchable Val where
   zipMatch = curry $ \ case
     (Int x, Int y) -> guard (x == y) $> Int x
+    (Tuple x, Tuple y) -> Tuple <$> zipMatch x y
+    _ -> Nothing
 
 instance Freezable a b m => Freezable (Val a) (Val b) m where
   freeze = traverse freeze
@@ -41,3 +47,15 @@ test1 = TestCase $ do
     _ <- readIVar y
     freeze' x
   z @?= Just [Known (Int 1), Known (Int 2)]
+
+test2 :: Test
+test2 = TestCase $ do
+  z <- runSupplyT $ runVerseT $ do
+    x <- freshVar
+    y <- freshVar
+    fork $ do
+      xs <- readIVar =<< all (x <$ (unify x =<< newVar (Int 1)))
+      unify y =<< newVar (Tuple xs)
+    unify x =<< newVar =<< pure (Int 1) <|> pure (Int 2)
+    freeze' y
+  z @?= Just [Known (Tuple [Known (Int 1)])]
