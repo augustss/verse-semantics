e6a2cfc15c0771bef80ab5a3f202ff5e78b0803e
Andy Sonnenburg <andy22286@gmail.com>
2023-08-29 22:55:17 -0400

    Delete VerseReferenceImpl/build-wasm.sh

--- VerseReferenceImpl/build-wasm.sh
+++
@@ -1,9 +0,0 @@
-wasm32-wasi-cabal install \
-  --installdir=www/static \
-  --allow-newer \
-  --overwrite-policy=always
-wizer \
-  --allow-wasi \
-  --wasm-bulk-memory true \
-  www/static/versewasm.wasm \
-  -o www/static/versewasm.wizer.wasm
