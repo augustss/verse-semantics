f9cc23cb2ea7c530ac0ae13a202bed5e31531da1
Ranjit Jhala <rjhala@eng.ucsd.edu>
2023-10-18 11:56:28 -0700

    add verifier text to desugaring.pdf

--- HaskellRules/FrontEnd/Flags.hs
+++ HaskellRules/FrontEnd/Flags.hs
@@ -50,7 +50,7 @@ defaultFlags = Flags
   , fTraceVerify  = False
   , fPrelude      = either error id $ findPrelude defaultPrelude
   , fNoWarn       = False
-  , fDesugar      = DS1
+  , fDesugar      = DS7
   }
 
 showFlags :: Flags -> String
