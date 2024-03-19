a8f85e4a0bb007ad37064d42503321fd36dc33ba
Andy Sonnenburg <andy22286@gmail.com>
2024-03-18 20:47:30 -0400

    Make wasm.bash use GHC 9.8.

--- VerseReferenceImpl/wasm.bash
+++ VerseReferenceImpl/wasm.bash
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -eux
+
 nix shell \
-  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_6 \
+  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_8 \
+  --extra-experimental-features nix-command \
+  --extra-experimental-features flakes \
+  --command \
+  wasm32-wasi-cabal update \
+
+nix shell \
+  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_8 \
   --extra-experimental-features nix-command \
   --extra-experimental-features flakes \
   --command \
@@ -12,7 +20,7 @@ wasm32-wasi-cabal install \
   -O2
 
 nix shell \
-  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_6 \
+  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_8 \
   --extra-experimental-features nix-command \
   --extra-experimental-features flakes \
   --command \
@@ -22,8 +30,10 @@ wizer \
   www/static/versewasm.wasm \
   -o www/static/versewasm.wizer.wasm
 
+cp www/static/versewasm.wasm www/static/versewasm.wizer.wasm
+
 nix shell \
-  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_6 \
+  https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz#all_9_8 \
   --extra-experimental-features nix-command \
   --extra-experimental-features flakes \
   --command \
