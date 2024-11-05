80ea8bdba0b2362c59227de9bfa375b844c77fb8
Lennart Augustsson <lennart.augustsson@epicgames.com>
2024-11-05 12:18:45 +0100

    Various cleanup and speedup.

--- verse-densem-new/Sem.hs
+++ verse-densem-new/Sem.hs
@@ -1,9 +1,13 @@
 {-# OPTIONS_GHC -Wall #-}
 module Main where
-import Data.List hiding (find)
+import Data.List
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Maybe
 --import Debug.Trace
+
+--------------------
+---- Abstract syntax
 
 type Ident = String
 
@@ -19,21 +23,36 @@ data Op = Oint | Ogt | Oadd
 data OC = Open | Closed
   deriving (Eq, Ord, Show)
 
+
+--------------------
+---- Values
+
 data Val = VInt Integer | VTup [Val] | VFcn (Fcn Val Val)
   deriving (Eq, Ord)
+
+data RVal = RVal Val | Wrong String
 
 instance Show Val where
   showsPrec p (VInt i) = showsPrec p i
   showsPrec p (VTup vs) = showsPrec p vs
   showsPrec p (VFcn f) = showsPrec p f
 
-data RVal = RVal Val | Wrong String
-
 instance Show RVal where
   showsPrec p (RVal v) = showsPrec p v
   showsPrec _ (Wrong s) = showString $ "Wrong(" ++ s ++ ")"
 
-data Fcn a b = Fcn String [(a, b)]    -- mapping from a to b
+vadd :: Val -> Val -> Val
+vadd (VInt x) (VInt y) = VInt ((x + y) `mod` maxVInt)
+vadd _ _ = undefined
+
+--------------------
+---- Functions as tables
+-- All functions have a unique name
+
+data Fcn a b = Fcn String (M.Map a b)    -- mapping from a to b
+
+mkFcn :: (Ord a) => String -> [(a, b)] -> Fcn a b
+mkFcn s xys = Fcn s (M.fromList xys)
 
 instance Eq (Fcn a b) where
   Fcn f _ == Fcn f' _  =  f == f'
@@ -44,49 +63,74 @@ instance Ord (Fcn a b) where
 instance Show (Fcn a b) where
   show (Fcn s _) = s
 
+-- Domain test
 inDom :: Ord a => a -> Fcn a b -> Bool
-inDom x (Fcn _ xys) = x `elem` map fst xys
+inDom x (Fcn _ xys) = M.member x xys
 
-newtype Set a = Set { unSet :: [a] }
+-- Application when the argument is in the domain
+ap :: (Show a, Ord a) => Fcn a b -> a -> b
+ap (Fcn f xys) x =
+  fromMaybe (error $ "ap: outside domain " ++ f ++ " " ++ show x) $
+  M.lookup x xys
 
-instance Show a => Show (Set a) where
-  showsPrec p (Set s) = showsPrec p s
+--------------------
+---- Sets
+
+type Set a = S.Set a
+
+unSet :: Set a -> [a]
+unSet = S.toList
+
+mkSet :: (Ord a) => [a] -> Set a
+mkSet = S.fromList
+
+sUnion :: (Ord a) => [Set a] -> Set a
+sUnion = S.unions
+
+isect :: Ord a => Set a -> Set a -> Set a
+isect = S.intersection
+
+sing :: a -> Set a
+sing = S.singleton
+
+unSing :: Set a -> a
+unSing s =
+  case unSet s of
+    [x] -> x
+    _   -> error "unSing"
+
+empty :: Set a
+empty = S.empty
+
+isEmpty :: Set a -> Bool
+isEmpty = S.null
+
+sIn :: Ord a => a -> Set a -> Bool
+sIn = S.member
+
+-- Check if a predicate holds for all values in the set
+forAll :: Set a -> (a -> Bool) -> Bool
+forAll xs p = all p (unSet xs)
+
+--------------------
+---- Environment
 
 type Env = M.Map Ident Val
 
-mkSet :: (Ord a) => [a] -> Set a
-mkSet = Set . remdup . sort
-  where remdup (x:x':xs) | x == x' = remdup (x:xs)
-                         | otherwise = x: remdup (x':xs)
-        remdup xs = xs
+lookupEnv :: Ident -> Env -> WS
+lookupEnv x rho = sing $ fromMaybe (error $ "lookupEnv: undefined " ++ show (x, rho)) $ M.lookup x rho
 
-sUnion :: (Ord a) => [Set a] -> Set a
-sUnion = mkSet . concatMap unSet
+-- Initial environment
+rho0 :: Env
+rho0 = M.fromList $
+  [ (n, unSing (dO o)) | (n, o) <- [("int", Oint), ("gt", Ogt), ("add", Oadd) ] ] ++
+  [ ("succ", VFcn fsucc), ("pred", VFcn fpred) ] ++
+  [ ("false", VTup []) ]
 
-{-
-sIsect :: (Ord a) => [Set a] -> Set a
-sIsect [] = undefined
-sIsect as = foldr1 isect as
--}
-
-isect :: Ord a => Set a -> Set a -> Set a
-isect s1 s2 = mkSet $ unSet s1 `intersect` unSet s2
-
-sing :: a -> Set a
-sing x = Set [x]
-
-unSing :: Set a -> a
-unSing (Set [x]) = x
-unSing _ = error "unSing"
-
-empty :: Set a
-empty = Set []
-
-isEmpty :: Set a -> Bool
-isEmpty (Set xys) = null xys
-
-sIn :: Ord a => a -> Set a -> Bool
-sIn x (Set xs) = x `elem` xs
+--------------------
+---- "Universal" set of values
+-- This is a carefully selected set of values to make
+-- the examples work.
 
 maxVInt :: Integer
 maxVInt = 4
@@ -94,12 +138,8 @@ maxVInt = 4
 allInts :: [Val]
 allInts = [ VInt i | i <- [0 .. maxVInt - 1] ]
 
-vadd :: Val -> Val -> Val
-vadd (VInt x) (VInt y) = VInt ((x + y) `mod` maxVInt)
-vadd _ _ = undefined
-
 allWs :: WS
-allWs = Set $
+allWs = S.fromList $
   nonFcn ++
   [ unSing (dO o) | o <- [Oint, Ogt, Oadd] ] ++
   map VFcn [ id0, id1, id01, f01, const0, const1, const2, const3, fsucc, fsucc2, fpred, comp, ho1, ho2, ho3 ]
@@ -107,113 +147,140 @@ allWs = Set $
     nonFcn =
       allInts ++
       [VTup [x, y] | x <- allInts, y <- allInts]
-    id0 = Fcn "id0" [(VInt 0, VInt 0)]
-    id1 = Fcn "id1" [(VInt 1, VInt 1)]
-    id01 = Fcn "id01" [(VInt 0, VInt 0), (VInt 1, VInt 1)]
-    f01 = Fcn "f01" [(VInt 0, VInt 0), (VInt 1, VInt 2)]
-    const0 = Fcn "const0" [(x, VInt 0) | x <- allInts]
-    const1 = Fcn "const1" [(x, VInt 1) | x <- allInts]
-    const2 = Fcn "const2" [(x, VInt 2) | x <- allInts]
-    const3 = Fcn "const3" [(x, VInt 3) | x <- allInts]
-    comp = Fcn "comparable" [(w, w) | w <- nonFcn ]
+    id0 = mkFcn "id0" [(VInt 0, VInt 0)]
+    id1 = mkFcn "id1" [(VInt 1, VInt 1)]
+    id01 = mkFcn "id01" [(VInt 0, VInt 0), (VInt 1, VInt 1)]
+    f01 = mkFcn "f01" [(VInt 0, VInt 0), (VInt 1, VInt 2)]
+    const0 = mkFcn "const0" [(x, VInt 0) | x <- allInts]
+    const1 = mkFcn "const1" [(x, VInt 1) | x <- allInts]
+    const2 = mkFcn "const2" [(x, VInt 2) | x <- allInts]
+    const3 = mkFcn "const3" [(x, VInt 3) | x <- allInts]
+    comp = mkFcn "comparable" [(w, w) | w <- nonFcn ]
     -- The function that accepts f:int->int as an argument and returns f[1]
-    ho1 = Fcn "ho1" [(VFcn fsucc, VInt 2), (VFcn fpred, VInt 0), (VFcn fint, VInt 1),
-                     (VFcn fsucc2, VInt 3), (VFcn comp, VInt 1),
-                     (VFcn const0, VInt 0), (VFcn const1, VInt 1), (VFcn const2, VInt 2), (VFcn const3, VInt 3)
-                    ]
-    ho2 = Fcn "ho2" [(VFcn fsucc, VInt 3), (VFcn fpred, VInt 1), (VFcn fint, VInt 2),
-                     (VFcn fsucc2, VInt 0), (VFcn comp, VInt 2),
-                     (VFcn const0, VInt 0), (VFcn const1, VInt 1), (VFcn const2, VInt 2), (VFcn const3, VInt 3)
-                    ]
-    ho3 = Fcn "ho3" [(VFcn fsucc, VInt 3), (VFcn fpred, VInt 1), (VFcn fint, VInt 2),
-                     (VFcn fsucc2, VInt 0), (VFcn comp, VInt 2),
-                     (VFcn const0, VInt 1), (VFcn const1, VInt 2), (VFcn const2, VInt 3), (VFcn const3, VInt 0)
-                    ]
+    ho1 = mkFcn "ho1" [(VFcn fsucc, VInt 2), (VFcn fpred, VInt 0), (VFcn fint, VInt 1),
+                       (VFcn fsucc2, VInt 3), (VFcn comp, VInt 1),
+                       (VFcn const0, VInt 0), (VFcn const1, VInt 1), (VFcn const2, VInt 2), (VFcn const3, VInt 3)
+                      ]
+    ho2 = mkFcn "ho2" [(VFcn fsucc, VInt 3), (VFcn fpred, VInt 1), (VFcn fint, VInt 2),
+                       (VFcn fsucc2, VInt 0), (VFcn comp, VInt 2),
+                       (VFcn const0, VInt 0), (VFcn const1, VInt 1), (VFcn const2, VInt 2), (VFcn const3, VInt 3)
+                      ]
+    ho3 = mkFcn "ho3" [(VFcn fsucc, VInt 3), (VFcn fpred, VInt 1), (VFcn fint, VInt 2),
+                       (VFcn fsucc2, VInt 0), (VFcn comp, VInt 2),
+                       (VFcn const0, VInt 1), (VFcn const1, VInt 2), (VFcn const2, VInt 3), (VFcn const3, VInt 0)
+                      ]
 
 fint :: Fcn Val Val
-fint = Fcn "int" [(x, x) | x <- allInts ]
+fint = mkFcn "int" [(x, x) | x <- allInts ]
 
 fsucc :: Fcn Val Val
-fsucc = Fcn "succ" [(x, vadd x (VInt 1)) | x <- allInts ]
+fsucc = mkFcn "succ" [(x, vadd x (VInt 1)) | x <- allInts ]
 
 fsucc2 :: Fcn Val Val
-fsucc2 = Fcn "succ2" [(x, vadd x (VInt 2)) | x <- allInts ]
+fsucc2 = mkFcn "succ2" [(x, vadd x (VInt 2)) | x <- allInts ]
 
 fpred :: Fcn Val Val
-fpred = Fcn "pred" [(x, vadd x (VInt 3)) | x <- allInts ]
+fpred = mkFcn "pred" [(x, vadd x (VInt 3)) | x <- allInts ]
 
-rho0 :: Env
-rho0 = M.fromList $
-  [ (n, unSing (dO o)) | (n, o) <- [("int", Oint), ("gt", Ogt), ("add", Oadd) ] ] ++
-  [ ("succ", VFcn fsucc), ("pred", VFcn fpred) ]
+--------------------
+---- Aux
 
 type W = Val
 type WS = Set W
 
-forAll :: Set a -> (a -> Bool) -> Bool
-forAll xs p = all p (unSet xs)
-
+-- Given an initial environment, rho, and some identifiers,
+-- generate all environments where rho has been extended with
+-- the given identifiers bound to all possible value.
 genRhos :: Env -> [Ident] -> [Env]
 genRhos rho xs = 
   let exts = sequence $ map (\ x -> map (x,) (unSet allWs)) xs
   in  map (foldr (uncurry M.insert) rho) exts
 
+-- Generate all possible environment extensions and then
+-- evaluate using the given semantic function, ev.
+-- Finally, take the union of all those.
 tryAll :: Env -> [Ident] -> (Env -> WS) -> WS
 tryAll rho xs ev = sUnion $ map ev $ genRhos rho xs
 
+-- Verse function application, handles tuples and functions.
+-- Returns a singleton set on success.
+-- If the function argument is neither of those, return the empty set.
+-- NOTE: if the non-function case gives an error, then the way
+-- we deal with existentials will not work since it generates
+-- a lot of non-functions.
 apply :: Val -> Val -> Set Val
 apply (VTup ws) (VInt k) | 0 <= k' && k' < length ws = sing (ws !! k')  where k' = fromInteger k
-apply (VFcn (Fcn _ xys)) w = maybe empty sing $ lookup w xys
+apply (VFcn (Fcn _ xys)) w = maybe empty sing $ M.lookup w xys
 apply _ _ = empty
 
-ap :: Ord a => Fcn a b -> a -> b
-ap (Fcn _ xys) x = fromMaybe undefined $ lookup x xys
-
-find :: Ident -> Env -> WS
-find x rho = sing $ fromMaybe (error $ "find: undefined " ++ show (x, rho)) $ M.lookup x rho
+--------------------
+---- Find all identifiers defined by := in this scope
 
 dI :: Exp -> [Ident]
-dI (App e1 e2) = dI e1 <> dI e2
-dI (Equ e1 e2) = dI e1 <> dI e2
-dI (Seq e1 e2) = dI e1 <> dI e2
-dI (Tup es) = concat (map dI es)
-dI (Def i e) = [i] <> dI e
-dI (Colon e) = dI e
-dI _ = []
+dI = checkDup . sort . dI'
+  where
+    checkDup (x:x':xs) | x == x' = error $ "Duplicate definition of " ++ x
+                       | otherwise = x : checkDup (x':xs)
+    checkDup xs = xs
 
+dI' :: Exp -> [Ident]
+dI' (App e1 e2) = dI' e1 ++ dI' e2
+dI' (Equ e1 e2) = dI' e1 ++ dI' e2
+dI' (Seq e1 e2) = dI' e1 ++ dI' e2
+dI' (Tup es) = concat (map dI' es)
+dI' (Def i e) = i : dI' e
+dI' (Colon e) = dI' e
+dI' _ = []
+
+--------------------
+---- Primitive functions
+
+dO :: Op -> WS
+dO Oint = sing $ VFcn $ mkFcn "int" [ (x, x) | x <- allInts ]
+dO Ogt  = sing $ VFcn $ mkFcn "gt"  [ (VTup [x, y], x) | x <- allInts, y <- allInts, x > y]
+-- add is a single function, not many as in the doc.
+dO Oadd = sing $ VFcn $ mkFcn "add" [ (VTup [x, y], vadd x y) | x <- allInts, y <- allInts]
+
+--------------------
+---- Semantic equations, valuation
+
+-- P, top level program
 dP :: Exp -> RVal
 dP e =
-  case dD e rho0 of
-    Set [w] -> RVal w
-    Set ws -> Wrong $ show ws
+  case unSet $ dD e rho0 of
+    [w] -> RVal w
+    ws  -> Wrong $ show ws
 
+-- P', top level program using dL
 dP' :: Exp -> RVal
 dP' e =
-  case sUnion [ dL e w rho0 | w <- unSet allWs ] of
-    Set [w] -> RVal w
-    Set ws -> Wrong $ show ws
+  case unSet $ sUnion [ dL e w rho0 | w <- unSet allWs ] of
+    [w] -> RVal w
+    ws  -> Wrong $ show ws
 
+-- D, expression in a scope
 dD :: Exp -> Env -> WS
 dD e rho = tryAll rho (dI e) (dE e)
 
+-- E, expression
 dE :: Exp -> Env -> WS
---dE e rho | trace ("dE " ++ show (e, rho)) False = undefined
--- dE e rho = dM e allWs rho
-dE (Var x) rho = find x rho
+-- Use the next line to avoid having equations for E.
+-- It is a massive slowdown:  5s to 8m
+--dE e rho = sUnion [ dM e w rho | w <- unSet allWs ]
+dE (Var x) rho = lookupEnv x rho
 dE (Int k) _rho = sing $ VInt k
 dE (Prim o) _rho = dO o
 dE (App e1 e2) rho = mkSet [ r | f <- unSet $ dE e1 rho, a <- unSet $ dE e2 rho, r <- unSet $ apply f a ]
 dE (Equ e1 e2) rho = dE e1 rho `isect` dE e2 rho
 dE (Seq e1 e2) rho = mkSet [ y | _x <- unSet $ dE e1 rho, y <- unSet $ dE e2 rho ]
-dE (Def x e) rho = find x rho `isect` dE e rho
+dE (Def x e) rho = lookupEnv x rho `isect` dE e rho
 dE (Colon e) rho = mkSet [ r | f <- unSet $ dE e rho, a <- unSet allWs, r <- unSet $ apply f a ]
 dE Fail _rho = empty
 dE (If e1 e2 e3) rho =
-  case unSet $ sUnion [ dB e1 w rho | w <- unSet allWs ] of
+  case unSet $ dC e1 rho of
     [] -> dE e3 rho
     rhos -> sUnion [ dE e2 rho' | rho' <- rhos ]
 dE (Tup es) rho = mkSet $ map VTup $ sequence $ map (\ e -> unSet (dE e rho)) es
-  --[ VPair x y | x <- unSet $ dE e1 rho, y <- unSet $ dE e2 rho ]
 dE (Fun q e1 e2) rho = mkSet
   [ VFcn f | VFcn f <- unSet allWs,
         forAll allWs $ \ x ->
@@ -223,29 +290,34 @@ dE (Fun q e1 e2) rho = mkSet
             else inDom x f && forAll rhos (\ rho' -> ap f x `sIn` dD e2 rho')
   ]
 
-dO :: Op -> WS
-dO Oint = sing $ VFcn $ Fcn "int" [ (x, x) | x <- allInts ]
-dO Ogt  = sing $ VFcn $ Fcn "gt"  [ (VTup [x, y], x) | x <- allInts, y <- allInts, x > y]
-dO Oadd = sing $ VFcn $ Fcn "add" [ (VTup [x, y], vadd x y) | x <- allInts, y <- allInts]
+-- Get all possible "solutions", i.e., assignments to the existentials in e.
+dC :: Exp -> Env -> Set Env
+dC e rho = mkSet [ rho' | rho' <- genRhos rho (dI e), not $ isEmpty $ dE e rho' ]
 
-dB :: Exp -> W -> Env -> Set Env
-dB e u rho = mkSet [ rho' | rho' <- genRhos rho (dI e), not $ isEmpty $ dM e u rho' ]
 
+--------------------
+---- Semantic equations, matching
+
+-- L, expression matching in a scope
+-- (Like D, but for M)
 dL :: Exp -> W -> Env -> WS
 dL e u rho = tryAll rho (dI e) (dM e u)
 
+-- M, expression matching
+-- Match the value u against the expression, returning all possible
+-- values of the expression that makes it match u.
 dM :: Exp -> W -> Env -> WS
-dM (Var x) u rho = find x rho `isect` sing u
+dM (Var x) u rho = lookupEnv x rho `isect` sing u
 dM (Int k) u _rho = sing (VInt k) `isect` sing u
 dM (Prim o) u _rho = dO o `isect` sing u
 dM (App e1 e2) u rho = mkSet [ r | f <- unSet $ dE e1 rho, a <- unSet $ dE e2 rho, r <- unSet $ apply f a ] `isect` sing u
 dM (Equ e1 e2) u rho = dM e1 u rho `isect` dM e2 u rho
 dM (Seq e1 e2) u rho = mkSet [ y | _x <- unSet $ dE e1 rho, y <- unSet $ dM e2 u rho ]
-dM (Def x e) u rho = find x rho `isect` dM e u rho
+dM (Def x e) u rho = lookupEnv x rho `isect` dM e u rho
 dM (Colon e) u rho = mkSet [ r | f <- unSet $ dE e rho, r <- unSet $ apply f u ]
 dM Fail _u _rho = empty
 dM (If e1 e2 e3) u rho =
-  case unSet $ sUnion [ dB e1 w rho | w <- unSet allWs ] of
+  case unSet $ dC e1 rho of
     [] -> dM e3 u rho
     rhos -> sUnion [ dM e2 u rho' | rho' <- rhos ]
 dM (Tup es) u rho | VTup us <- u, length us == length us =
@@ -263,7 +335,15 @@ dM (Fun q e1 e2) u rho = mkSet
                                                                   ap f x `sIn` dL e2 (ap g x') rho'))
   ]
 
------
+-- Solve
+-- (Like C, but for M)
+dB :: Exp -> W -> Env -> Set Env
+dB e u rho = mkSet [ rho' | rho' <- genRhos rho (dI e), not $ isEmpty $ dM e u rho' ]
+
+
+
+--------------------
+---- Examples
 
 -- x:=2; y:=1; add[(x,y)]
 exp1 :: Exp
@@ -398,11 +478,26 @@ exp19 = App exp17 (Fun Closed (Colon (Var "int")) (Int 0))
 ex19 :: RVal
 ex19 = dP exp19
 
+-- if (1=1){2}else{0}
+exp20 :: Exp
+exp20 = If (Int 1 `Equ` Int 1) (Int 2) (Int 0)
+
+-- if (1=3){2}else{0}
+exp21 :: Exp
+exp21 = If (Int 1 `Equ` Int 3) (Int 2) (Int 0)
+
+-- if (x:int){x}{999} = 3
+exp22 :: Exp
+exp22 = If (Def "x" (Colon (Var "int"))) (Var "x") (Int 999) `Equ` Int 3
+
 allExps :: [Exp]
-allExps = [exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10, exp11, exp12, exp13, exp14, exp15, exp16, exp17, exp18, exp19]
+allExps = [exp1, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9,
+           exp10, exp11, exp12, exp13, exp14, exp15, exp16, exp17, exp18, exp19,
+           exp20, exp21, exp22
+          ]
 
 refExps :: String
-refExps = "[3,int,Wrong([comparable,int]),succ,3,1,ho1,2,1,2,ho2,2,Wrong([]),Wrong([]),0,0,ho3,2,1]"
+refExps = "[3,int,Wrong([comparable,int]),succ,3,1,ho1,2,1,2,ho2,2,Wrong([]),Wrong([]),0,0,ho3,2,1,2,0,3]"
 
 allOK :: Bool
 allOK = show (map dP allExps) == refExps
