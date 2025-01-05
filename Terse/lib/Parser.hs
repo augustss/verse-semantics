50a6b635cdcfd7e938a358ed00ac466dac784095
Andy Sonnenburg <andy22286@gmail.com>
2025-01-04 20:37:57 -0500

    Row and column upon parse failure.

--- Terse/lib/Parser.hs
+++ Terse/lib/Parser.hs
@@ -46,42 +46,40 @@ type Yield r = (Text -> r) -> r
 
 type Succeed r a = a -> S -> [Text] -> Fail r -> r
 
-type Fail r = Text -> [Text] -> r
+type Fail r = Text -> Int -> Int -> [Text] -> r
 
 data Result a
   = Yield (Text -> Result a)
   | Pure a
-  | Empty [Text]
+  | Empty {-# UNPACK #-} !Int {-# UNPACK #-} !Int [Text]
 
 runParser :: Parser a -> Text -> Result a
 runParser m input =
   let
     s = S { input, indexWord8 = 0, rowIndexWord8 = 0, row = 0, column = 0 }
   in
-    unParser m s [] yk sk fk ak
+    unParser m s [] yk sk fk fk
   where
     yk = Yield
     sk x _s _ann _fk = Pure x
     fk _input = Empty
-    ak _input = Empty
 
-parse :: Parser a -> Text -> Either [Text] a
+parse :: Parser a -> Text -> Either (Int, Int, [Text]) a
 parse m input =
   let
     s = S { input, indexWord8 = 0, rowIndexWord8 = 0, row = 0, column = 0 }
   in
-    unParser m s [] yk sk fk ak
+    unParser m s [] yk sk fk fk
   where
     yk f = f mempty
     sk x _s _ann _fk = Right x
-    fk _input = Left
-    ak _input = Left
+    fk _input row column ann = Left (row, column, ann)
 
-done :: Result a -> Either [Text] a
+done :: Result a -> Either (Int, Int, [Text]) a
 done = \ case
   Yield f -> done $ f mempty
   Pure x -> Right x
-  Empty e -> Left e
+  Empty row column ann -> Left (row, column, ann)
 
 step :: Result a -> Text -> Result a
 step = \ case
@@ -104,10 +102,10 @@ instance Applicative Parser where
 
 instance Alternative Parser where
   empty = Parser $ \ s ann _yk _sk fk _ak ->
-    fk s.input ann
+    fk s.input s.row s.column ann
   x <|> y = Parser $ \ s ann yk sk fk ak ->
     unParser x s ann yk sk
-    (\ input _ann -> unParser y s { input } ann yk sk fk ak)
+    (\ input _row _column _ann -> unParser y s { input } ann yk sk fk ak)
     ak
 
 infixl 0 <?>
@@ -130,12 +128,12 @@ char x = Parser $ \ s ann yk sk fk ak ->
           { indexWord8 = s.indexWord8 + i
           , column = s.column + 1
           } ann ak
-        else fk s.input ann
+        else fk s.input s.row s.column ann
   in
     if Unsafe.lengthWord8 s.input == s.indexWord8
     then yk $ \ input ->
       if Text.null input
-      then fk s.input ann
+      then fk s.input s.row s.column ann
       else loop s { input = s.input <> input }
     else loop s
 
@@ -151,12 +149,12 @@ tab = Parser $ \ s ann yk sk fk ak ->
           { indexWord8 = s.indexWord8 + i
           , column = s.column + 8
           } ann ak
-        else fk s.input ann
+        else fk s.input s.row s.column ann
   in
     if Unsafe.lengthWord8 s.input == s.indexWord8
     then yk $ \ input ->
       if Text.null input
-      then fk s.input ann
+      then fk s.input s.row s.column ann
       else loop s { input = s.input <> input }
     else loop s
 
@@ -174,12 +172,12 @@ newline = Parser $ \ s ann yk sk fk ak ->
           , row = s.row + 1
           , column = 0
           } ann ak
-        else fk s.input ann
+        else fk s.input s.row s.column ann
   in
     if Unsafe.lengthWord8 s.input == s.indexWord8
     then yk $ \ input ->
       if Text.null input
-      then fk s.input ann
+      then fk s.input s.row s.column ann
       else loop s { input = s.input <> input }
     else loop s
 
@@ -189,5 +187,5 @@ eof = Parser $ \ s ann yk sk fk _ak ->
   then yk $ \ input ->
     if Text.null input
     then sk () s ann fk
-    else fk (s.input <> input) ann
-  else fk s.input ann
+    else fk (s.input <> input) s.row s.column ann
+  else fk s.input s.row s.column ann
