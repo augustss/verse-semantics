1adc090fb2dc3c80170dc370f865ba2b304ee760
Lennart Augustsson <lennart.augustsson@epicgames.com>
2025-06-18 13:31:44 +0200

    First version of M

--- Spec/PlanA.hs
+++ Spec/PlanA.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE MonadComprehensions #-}
-module PlanA where
+{-# LANGUAGE OverloadedStrings #-}
+module Main {-PlanA-} where
+import Control.Monad(zipWithM)
 import Data.List(dropWhileEnd)
 import GHC.Stack
 import Exp
@@ -11,13 +14,15 @@ import Debug.Trace
 
 import ExpSugar
 
+type VAL = SetX Val
+
 dA :: Exp -> Env -> Val
 dA (Var x)             rho  = lookupVar x rho
 dA (Int k)            _rho  = VInt k
 dA (Prim p)           _rho  = dO p
 dA _                  _     = error "dA"
 
-dE :: Exp -> Env -> WS
+dE :: Exp -> Env -> [VAL]
 dE (Var "_")          _rho  = [allWs]
 dE e@Var{}             rho  = [sing $ dA e rho]
 dE e@Int{}             rho  = [sing $ dA e rho]
@@ -34,41 +39,41 @@ dE (If e1 e2 e3)       rho  =
   in  if isEmpty rhos then
         dD e3 rho
       else
-        unionHat [ dD e2 rho' | rho' <- rhos ]
+        unionsHat [ dD e2 rho' | rho' <- rhos ]
 -- Fun
 dE (Choice e1 e2)      rho  = dD e1 rho ++ dD e2 rho
 dE (All e)             rho  = [ fmap mkTup $ sequence $ squash $ dD e rho ]
 -- For
-dE (Where e1 e2)       rho  = [ s1 | s1 <- ws1, s2 <- ws2, not (isEmpty s2) ]
-  where ws1 = dE e1 rho; ws2 = dE e2 rho
-
+dE (Where e1 e2)       rho  = [ if isEmpty s2 then empty else s1
+                              | s1 <- dE e1 rho, s2 <- dE e2 rho ]
 dE (Def2 x y e)        rho  = [sing $ lookupVar x rho] `eequ` [sing $ lookupVar y rho] `eequ` dE e rho
 -- OfType
-dE (UChoice e1 e2)     rho  = [ s1 `union` s2 | s1 <- dD e1 rho, s2 <- dD e2 rho ]
+dE (UChoice e1 e2)     rho  = unionHat (dD e1 rho) (dD e2 rho)
 dE (Block e)           rho  = dD e rho
 dE (DefI x e)          rho  = [sing $ lookupVar x rho] `eequ` dE e rho
 dE (Exi _)            _rho  = [sing $ VInt 99999]
-dE (Fun q e1 e2)       rho  = trim [
+dE (Fun q e1 e2)       rho  = {-trim-} [
     [ f | f@VFcn{} <- allWs {-mkSetUnsafe [mkVFcn [(VInt 1,VInt 0),(VInt 2,VInt 0)]]-}
         , let rhos = dX e1 rho
-        , forAll {-allInts-} allWs $ \ x ->
+        , forAll allWs {-allInts-} $ \ x ->
             forAll rhos $ \ rho' ->
               let d1 = dM e1 x rho' in
---              trace ("try f=" ++ show f ++ ", x=" ++ show x ++ ", d1=" ++ show d1) $
-              (not $ null d1) `implies`
+              --trace ("try f=" ++ show f ++ ", x=" ++ show x ++ ", d1=" ++ show d1) $
+              (notFail d1) `implies`
               (let fx = apply f x in
                not (null fx) &&
-                ( -- trace ("fx=" ++ show fx ++ ", d2=" ++ show (d1 `eseq` dD e2 rho')) $
+                ( --trace ("fx=" ++ show fx ++ ", d2=" ++ show (d1 `eseq` dD e2 rho')) $
                 fx `elemSeq` (d1 `eseq` dD e2 rho'))  )
         , (q == Closed) `implies`
           (forAll (domV f) $ \ x ->
              exists rhos $ \ rho' ->
-               not $ null $ dM e1 x rho')
+               notFail $ dM e1 x rho')
     ]
   ]
+dE e _ = error $ "dE: " ++ show e
 
-dD :: Exp -> Env -> WS
-dD e rho = unionHat [ dE e rho' | rho' <- dX e rho ]
+dD :: Exp -> Env -> [VAL]
+dD e rho = unionsHat [ dE e rho' | rho' <- dX e rho ]
 
 dX :: Exp -> Env -> SetX Env
 dX e rho = mkSetUnsafe $ dXL e rho
@@ -84,13 +89,13 @@ dC e rho = [ rho' | rho' <- dX e rho, not $ null $ squash $ dD e rho' ]
 
 
 
-eseq :: WS -> WS -> WS
+eseq :: [VAL] -> [VAL] -> [VAL]
 eseq ws1 ws2 = [ if isEmpty s1 then empty else s2 | s1 <- ws1, s2 <- ws2 ]
 
-eequ :: WS -> WS -> WS
-eequ ws1 ws2 = trim [ s1 `intersect` s2 | s1 <- ws1, s2 <- ws2 ]
+eequ :: [VAL] -> [VAL] -> [VAL]
+eequ ws1 ws2 = {-trim-} [ s1 `intersect` s2 | s1 <- ws1, s2 <- ws2 ]
 
-eapp :: WS -> WS -> WS
+eapp :: [VAL] -> [VAL] -> [VAL]
 eapp ws1 ws2 = [ r | s1 <- ws1, s2 <- ws2, r <- applyFsAs s1 s2 ]
 
 lookupVar :: Ident -> Env -> W
@@ -102,7 +107,7 @@ lookupVar x rho =
         Just f -> VFcn [f]
         Nothing -> error $ "undefined variable " ++ show x
 
-elemSeq :: WS -> WS -> Bool
+elemSeq :: [VAL] -> [VAL] -> Bool
 elemSeq [] _ = True
 elemSeq _ [] = False
 elemSeq (x:xs) (y:ys) = x `isSubsetOf` y && elemSeq xs ys
