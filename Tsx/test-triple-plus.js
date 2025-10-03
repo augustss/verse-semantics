3836ae6042517ceee30836c91f3bedc6d1b04b13
Jan Vitek <1592581+janvitek@users.noreply.github.com>
2025-10-03 16:23:12 +0200

    Clean up

--- Tsx/test-triple-plus.js
+++
@@ -1,9 +0,0 @@
-const { parseExpression } = require('./dist/index.js');
-
-console.log('Testing: +++');
-try {
-  const ast = parseExpression('+++');
-  console.log('Parsed as:', JSON.stringify(ast, null, 2));
-} catch (e) {
-  console.log('Failed:', e.message);
-}
