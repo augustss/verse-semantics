0f0ab5a1eba0932d5334c5832e1ca4e64ccbd83a
Andy Sonnenburg <andy22286@gmail.com>
2025-11-15 09:50:33 -0500

    `MonadWeakRef`, but with too great of cost to reduce memory usage of trailing.

--- Terse/lib/Ref.hs
+++ Terse/lib/Ref.hs
@@ -1,8 +1,13 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UnboxedTuples #-}
 module Ref
-  ( MonadRef (..)
-  , Ref
+  ( Ref
   , World
+  , MonadRef (..)
+  , WeakRef
+  , MonadWeakRef (..)
   ) where
 
 import Control.Monad.ST
@@ -12,6 +17,11 @@ import Control.Monad.Trans.State.Strict qualified as Strict
 import Data.Kind
 import Data.STRef
 
+import GHC.IO
+import GHC.Prim
+import GHC.STRef
+import GHC.Weak
+
 type Ref m = STRef (World m)
 
 type family World (m :: Type -> Type)
@@ -20,6 +30,12 @@ class Monad m => MonadRef m where
   newRef :: a -> m (Ref m a)
   readRef :: Ref m a -> m a
   writeRef :: Ref m a -> a -> m ()
+
+type WeakRef = Weak
+
+class MonadRef m => MonadWeakRef m where
+  newWeakRef :: Ref m a -> b -> m (WeakRef b)
+  readWeakRef :: WeakRef a -> m (Maybe a)
 
 type instance World IO = RealWorld
 
@@ -35,9 +51,19 @@ instance MonadRef (ST s) where
   readRef = readSTRef
   writeRef = writeSTRef
 
+instance MonadWeakRef IO where
+  newWeakRef (STRef var) x = IO $ \ s ->
+    case mkWeakNoFinalizer# var x s of
+      (# s, x #) -> (# s, Weak x #)
+  readWeakRef = deRefWeak
+
 type instance World (Strict.StateT s m) = World m
 
 instance MonadRef m => MonadRef (Strict.StateT s m) where
   newRef = lift . newRef
   readRef = lift . readRef
   writeRef = (lift .) . writeRef
+
+instance MonadWeakRef m => MonadWeakRef (Strict.StateT s m) where
+  newWeakRef = (lift .) . newWeakRef
+  readWeakRef = lift . readWeakRef
