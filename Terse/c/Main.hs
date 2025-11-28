1a09fa5e02ab7ae360d3d2c7bbc56df0006eb5d4
Andy Sonnenburg <andy22286@gmail.com>
2025-11-27 19:25:07 -0500

    Make tersec work as a compiler, though it requires `terse` to be installed globally.

--- Terse/c/Main.hs
+++ Terse/c/Main.hs
@@ -1,12 +1,46 @@
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main
   ( main
   ) where
 
-import Prettyprinter
+import Data.Foldable
+import Data.Text (Text)
+import Data.Text.IO qualified as Text
 
-import Verse.TH
+import GHC.Exts
+
+import System.Environment
+import System.Exit
+import System.FilePath
+import System.Process
 
 main :: IO ()
-main = print . pretty =<< [verseFile|examples/1a.verse|]
+main = getArgs >>= traverse_ f
+  where
+    f verseFilePath = do
+      let hsFilePath = replaceExtension verseFilePath "hs"
+      Text.writeFile hsFilePath (contents verseFilePath)
+      exitWith =<< rawSystem "ghc"
+        [ "--make"
+        , "-Wall"
+        , "-O2"
+        , "-package", "prettyprinter"
+        , "-package", "terse"
+        , hsFilePath
+        ]
+
+contents :: String -> Text
+contents x = "\
+\{-# OPTIONS_GHC -Wno-unused-matches #-}\n\
+\{-# LANGUAGE QuasiQuotes #-}\n\
+\module Main\n\
+\  ( main\n\
+\  ) where\n\
+\\n\
+\import Prettyprinter\n\
+\\n\
+\import Verse.TH\n\
+\\n\
+\main :: IO ()\n\
+\main = print . pretty =<< [verseFile|" <> fromString x <> "|]\n\
+\"
