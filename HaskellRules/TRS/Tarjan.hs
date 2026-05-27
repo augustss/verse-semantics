c7f7c6a126f9434811566439887268c69abbdd35
Koen Claessen <koen@chalmers.se>
2026-05-27 10:15:25 +0200

    a small implementation of the Timbda calculus in Haskell

---
+++ HaskellRules/TRS/Tarjan.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveFunctor #-}
+module TRS.Tarjan(tarjan1, tarjan, tarjanAny, Result (..), fromResult, isTimeout, finish) where
+
+import qualified Data.Map as M
+import Data.Map( (!) )
+import qualified Data.Set as S
+
+import Epic.List(takeUntil, dropUntil)
+
+-----------------------------------------------------------------------------------
+
+-- Tarjans SCC algorithm, it's lazy in the graph, so you can use it just find the
+-- first component and you don't have to build the whole graph!
+
+-- reimplemented from
+-- https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+
+type Fuel = Int
+type Kont a = Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> Result [[a]]
+
+------------------------------------------------------------------------------------
+
+data Result a = Timeout a | Finish a deriving (Eq, Ord, Functor)
+
+fromResult :: a -> Result a -> a
+fromResult _   (Finish v)  = v
+fromResult def (Timeout _) = def
+
+isTimeout :: Result a -> Bool
+isTimeout (Timeout _) = True
+isTimeout _ = False
+
+finish :: b -> (a -> b) -> Result a -> b
+finish def _ (Timeout _) = def
+finish _   f (Finish x)  = f x
+
+------------------------------------------------------------------------------------
+
+-- Return one normal form.
+tarjan1 :: forall a . Ord a => Fuel -> (a -> [a]) -> a -> Result [a]
+tarjan1 afuel nexts a = hd <$> tarjanAny True afuel nexts a
+  where hd [x] = x
+        hd _ = undefined  -- This should never happen
+
+-- Return all normal forms.
+tarjan :: forall a . Ord a => Fuel -> (a -> [a]) -> a -> Result [[a]]
+tarjan = tarjanAny False
+
+-- If justOne is True then the returned list is always a singleton.
+tarjanAny :: forall a . Ord a => Bool -> Fuel -> (a -> [a]) -> a -> Result [[a]]
+tarjanAny justOne afuel nexts x = strongc afuel 0 M.empty S.empty [] x (\ _ _ _ _ zs -> Timeout [zs])
+ where
+  strongc :: Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> a -> Kont a -> Result [[a]]
+  strongc fuel !index state onStack stack v k =
+    visit (fuel-1) (index+1) (M.insert v (index,index) state) (S.insert v onStack) (v:stack) v (nexts v) k
+
+  visit :: Fuel -> Int -> M.Map a (Int,Int) -> S.Set a -> [a] -> a -> [a] -> Kont a -> Result [[a]]
+  visit fuel _ _ _ stack _ _ _ | fuel <= 0 =
+    Timeout [stack]
+
+  visit fuel !index state onStack stack v [] k =
+    if vindex == vlowlink then
+      let xs = takeUntil (v==) stack in
+        if justOne then
+          Finish [xs]
+        else
+          (xs :) <$> k fuel index state (foldr S.delete onStack xs) (dropUntil (v==) stack)
+    else
+      k fuel index state onStack stack
+   where
+    (vindex, vlowlink) = state!v
+
+  visit fuel index state onStack stack v (w:ws) k =
+    case M.lookup w state of
+      Nothing ->
+        strongc fuel index state onStack stack w $
+          \ fuel' index' state' onStack' stack' ->
+            let (vindex, vlowlink) = state' ! v
+                (_windex, wlowlink) = state' ! w
+             in visit (fuel'-1) index' (M.insert v (vindex, vlowlink `min` wlowlink) state')
+                      onStack' stack' v ws k
+
+      Just (windex, _wlowlink) ->
+        if w `S.member` onStack then
+          visit (fuel-1) index (M.insert v (vindex, vlowlink `min` windex) state) onStack stack v ws k
+        else
+          visit (fuel-1) index state onStack stack v ws k
+       where
+        (vindex, vlowlink) = state ! v
+
+{-
+main = print $ head $ tarjan f 1
+f x | x < 1000000 = [x+1,3*x,x+100]
+    | otherwise   = [x `div` 2]
+-}
---
+++ Spec/Timbda.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE GADTs #-}
+module Timbda where
+
+import Prelude hiding ( pi )
+
+---------------------------------------------------------------------------
+-- identifiers and environments
+
+data Ident env a where
+  This :: Ident (env,a) a
+  That :: Ident env a -> Ident (env,b) a
+
+data Env env where
+  Empty :: Env ()
+  Add   :: a -> Env env -> Env (env,a)
+
+look :: Ident env a -> Env env -> a
+look This     (Add a _)   = a
+look (That x) (Add _ env) = look x env
+
+---------------------------------------------------------------------------
+-- semantic types
+
+-- naturals
+type Nat   = Int
+
+-- function types
+type a:->b = [(a,b)]
+
+pi :: [a] -> (a->[b]) -> [a:->b]
+pi []     f = [ [] ]
+pi (a:as) f = [ (a,b):abs | b <- f a, abs <- pi as f ]
+
+---------------------------------------------------------------------------
+-- expressions in Timbda calculus
+
+data Expr env a where
+  Con   :: Nat -> Expr env Nat
+  Var   :: Ident env a -> Expr env a
+  Nat   :: Expr env (Nat:->Nat)
+  Lam   :: Expr env a -> Expr (env,a) b -> Expr env (a:->b)
+  App   :: Eq a => Expr env (a:->b) -> Expr env a -> Expr env b
+  Img   :: Expr env (a:->b) -> Expr env b -- :e
+  Fail  :: Expr env a
+  (:|:) :: Expr env a -> Expr env a -> Expr env a  
+  
+eval :: Env env -> Expr env a -> [a]
+eval env (Con k)     = [k]
+eval env (Var x)     = [look x env]
+eval env Nat         = [[(a,a)|a<-[0..5]]] -- 5 is the largest number
+eval env (Lam e1 e2) = pi (eval env e1) (\a -> eval (Add a env) e2)
+eval env (App e1 e2) = [b | f <- eval env e1, (a,b) <- f, a' <- eval env e2, a==a']
+eval env (Img e)     = [b | f <- eval env e, (a,b) <- f]
+eval env Fail        = []
+eval env (e1 :|: e2) = eval env e1 ++ eval env e2
+
+---------------------------------------------------------------------------
+-- examples
+
+ex1 = Lam (Img Nat) (Img Nat)
+
+---------------------------------------------------------------------------
+
