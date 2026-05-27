b315ac2a54b09659f1cabd6daa723653243ed8dd
Koen Claessen <koen@chalmers.se>
2026-05-27 14:18:13 +0200

    add unification

--- Spec/Timbda3.hs
+++ Spec/Timbda3.hs
@@ -39,23 +39,65 @@ pi (a:dom) rng = [ (a,b):f | b <- rng a, f <- pi dom rng ]
 -- expressions in Timbda calculus
 
 data Expr env env' a b where
-  Con   :: Nat -> Expr env env Nat Nat
-  Var   :: Ident env u -> Expr env env u u
+  -- k
+  Con   :: Nat
+        -> Expr env env Nat Nat
+
+  -- x
+  Var   :: Ident env u
+        -> Expr env env u u
+
+  -- nat
   Nat   :: Expr env env (Nat:->Nat) (Nat:->Nat)
-  Lam   :: Expr env env1 a b -> Expr env1 env2 c d -> Expr env env (b:->c) (a:->d)
-  Def   :: Expr env env1 u v -> Expr env (env1,v) u v
-  (:>:) :: Expr env1 env2 i j -> Expr env2 env3 u v -> Expr env1 env3 u v
-  App   :: Eq a => Expr env env1 i1 (a:->b) -> Expr env1 env2 i2 a -> Expr env env2 b b
-  Tup   :: Expr env env1 a b -> Expr env1 env2 c d -> Expr env env2 (a,c) (b,d)
-  Typ   :: Expr env env1 i (a:->b) -> Expr env env1 a b -- :e
-  Fail  :: Expr env env1 a b
-  (:|:) :: Expr env env1 a b -> Expr env env2 a b -> Expr env env a b
+
+  -- fun(e1){e2}
+  Fun   :: Expr env0 env1 a b
+        -> Expr env1 env2 c d
+        -> Expr env0 env0 (b:->c) (a:->d)
+
+  -- x:=e  (but x is only visible towards the right) (x is a deBrijn-index)
+  Def   :: Expr env1 env2 u v
+        -> Expr env1 (env2,v) u v
+
+  -- e1;e2
+  (:>:) :: Expr env1 env2 i j
+        -> Expr env2 env3 u v
+        -> Expr env1 env3 u v
+
+  -- e1[e2]
+  App   :: Eq a
+        => Expr env1 env2 i (a:->b)
+        -> Expr env2 env3 j a
+        -> Expr env1 env3 b b
+
+  -- <e1,e2>
+  Tup   :: Expr env1 env2 a b
+        -> Expr env2 env3 c d
+        -> Expr env1 env3 (a,c) (b,d)
+
+  -- :e
+  Typ   :: Expr env1 env2 i (a:->b)
+        -> Expr env1 env2 a b
+
+  -- fail
+  Fail  :: Expr env1 env2 a b
+
+  -- e1|e2
+  (:|:) :: Expr env0 env1 a b
+        -> Expr env0 env2 a b
+        -> Expr env0 env0 a b
+
+  -- e1=e2
+  (:=:) :: (Eq a,Eq b)
+        => Expr env1 env2 a b
+        -> Expr env2 env3 a b
+        -> Expr env1 env3 a b
 
 eval :: Env env -> Expr env env' u v -> Rel (Env env',u,v)
 eval env (Con k)     = [(env,k,k)]
 eval env (Var x)     = [(env,u,u)] where u=look x env
 eval env Nat         = [(env,h,h)] where h=[(n,n)|n<-[0..5]]
-eval env (Lam e1 e2) = [ ( env
+eval env (Fun e1 e2) = [ ( env
                          , [(b,c) | ((_,_,b),(_,c,_)) <- h]
                          , [(a,d) | ((_,a,_),(_,_,d)) <- h]
                          )
@@ -80,20 +122,26 @@ eval env (Tup e1 e2) = [ (env2,(a,c),(b,d))
                        , (env2,c,d) <- eval env1 e2
                        ]
 eval env (Typ e)     = [ (env1,a,b)
-                       | (env1,_,f)  <- eval env e
-                       , (a,b)       <- f
+                       | (env1,_,f) <- eval env e
+                       , (a,b)      <- f
                        ]
 eval env Fail        = []
 eval env (e1:|:e2)   = [ (env,a,b) | (_,a,b) <- eval env e1 ] ++
                        [ (env,a,b) | (_,a,b) <- eval env e2 ]
+eval env (e1:=:e2)   = [ (env2,u1,v2)
+                       | (env1,u1,v1) <- eval env e1
+                       , (env2,u2,v2) <- eval env1 e2
+                       , u1==u2
+                       , v1==v2
+                       ]
 
 ---------------------------------------------------------------------------
 -- examples
 
-ex1 = Lam (Typ Nat) (Typ Nat)          -- fun(:nat){:nat}
-ex2 = Lam (Def (Typ Nat)) (Var This)   -- fun(x:=(:nat)){x}
-
-
+ex1 = Fun (Typ Nat) (Typ Nat)          -- fun(:nat){:nat}
+ex2 = Fun (Def (Typ Nat)) (Var This)   -- fun(x:=(:nat)){x}
+ex3 = Fun (Tup (Def (Typ Nat)) (Def (Typ Nat))) (Var (That This))
+      -- fun(x:nat,y:nat){x}
 
 ---------------------------------------------------------------------------
 
